86d3f652e470307d41490837faa58970e20c75da
Shane Becker <veganstraightedge@gmail.com>
2016-09-23 13:11:55 -0700

    Add background images to seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,8 @@ if Rails.env.development?
     content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     status: "published",
     published_at: 6.days.ago,
-    slug: "first-things-first"
+    slug: "first-things-first",
+    image: "https://http.cat/301.jpg"
   )
 
   article_with_css = Article.create!(
@@ -21,7 +22,8 @@ if Rails.env.development?
     content: "Notice, there's <b>no</b> subtitle. That's OK too. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     status: "published",
     published_at: 5.days.ago,
-    slug: "next-things-next"
+    slug: "next-things-next",
+    image: "https://http.cat/302.jpg"
   )
   article_with_css.css = "#article-#{article_with_css.id} b { background: red; }"
   article_with_css.save!
@@ -31,7 +33,8 @@ if Rails.env.development?
     subtitle: "This Time with a Subtitle Again",
     content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     status: "published",
-    published_at: 4.days.ago
+    published_at: 4.days.ago,
+    image: "https://http.cat/303.jpg"
   )
 
   Article.create!(
@@ -39,7 +42,8 @@ if Rails.env.development?
     subtitle: "Subtiles Are Another Thing Altogether",
     content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     status: "published",
-    published_at: 3.days.ago
+    published_at: 3.days.ago,
+    image: "https://http.cat/304.jpg"
   )
 
   Article.create!(
@@ -47,7 +51,8 @@ if Rails.env.development?
     subtitle: "Just for the Sake of Testing",
     content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     status: "published",
-    published_at: 2.days.ago
+    published_at: 2.days.ago,
+    image: "https://http.cat/305.jpg"
   )
 
   Article.create!(
@@ -55,7 +60,8 @@ if Rails.env.development?
     subtitle: "Because It's the Sixth Most Recent",
     content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     status: "published",
-    published_at: 7.days.ago
+    published_at: 7.days.ago,
+    image: "https://http.cat/401.jpg"
   )
 
   top_pinned_article = Article.create!(
@@ -68,13 +74,24 @@ if Rails.env.development?
     pinned_to_top: true
   )
 
+  bottom_pinned_article = Article.create!(
+    title: "Featured Project",
+    subtitle: "Underneath The Feed",
+    content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\n\nLorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    status: "published",
+    published_at: 1.days.ago,
+    slug: "featured-project",
+    pinned_to_bottom: true
+  )
+
   unpublished_article = Article.create!(
     title: "Something for the Future",
     subtitle: "Still Being Written, Edited, etc",
     content: "Only after an article is dated and marked published does it go out. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     status: "draft",
     published_at: 2.days.ago,
-    slug: "unpublished-article"
+    slug: "unpublished-article",
+    image: "https://http.cat/404.jpg"
   )
 
   page = Article.create!(
@@ -83,6 +100,7 @@ if Rails.env.development?
     status: "published",
     published_at: 10.days.ago,
     page: true,
-    page_path: "about/us"
+    page_path: "about/us",
+    image: "https://http.cat/502.jpg"
   )
 end
