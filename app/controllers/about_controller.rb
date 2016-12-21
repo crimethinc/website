9ec2fa2d7cc002dd352d5bfef4274617f398a689
Shane Becker <veganstraightedge@gmail.com>
2016-12-20 16:17:08 -0800

    WIP: placeholder for /read /watch /listen pages

---
+++ app/controllers/about_controller.rb
@@ -0,0 +1,10 @@
+class AboutController < ApplicationController
+  def read
+  end
+
+  def watch
+  end
+
+  def listen
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -53,22 +53,27 @@ Rails.application.routes.draw do
     resources :sessions, only: [:create]
   end
 
-  get "profile",  to: "auth/users#show",       as: "profile"
-  get "settings", to: "auth/users#edit",       as: "settings"
-  get "signup",   to: "auth/users#new",        as: "signup"
-  get "signin",   to: "auth/sessions#new",     as: "signin"
-  get "signout",  to: "auth/sessions#destroy", as: "signout"
+  get "profile",  to: "auth/users#show",       as: :profile
+  get "settings", to: "auth/users#edit",       as: :settings
+  get "signup",   to: "auth/users#new",        as: :signup
+  get "signin",   to: "auth/sessions#new",     as: :signin
+  get "signout",  to: "auth/sessions#destroy", as: :signout
 
-  # Wordpress admin URL redirects
-  get "wp-admin.php", to: redirect("/admin")
-  get "wp-login.php", to: redirect("/signin")
-  get "wp-login.php?action=logout&_wpnonce=:nonce", to: redirect("/signout")
+
+  # Pages
+  get "read",   to: "about#read",   as: :read
+  get "watch",  to: "about#watch",  as: :watch
+  get "listen", to: "about#listen", as: :listen
 
 
   # Misc plumbing infrastructure
   get "manifest.json",  to: "misc#mainfest_json"
   get "opensearch.xml", to: "misc#opensearch_xml"
 
+  # Wordpress admin URL redirects
+  get "wp-admin.php", to: redirect("/admin")
+  get "wp-login.php", to: redirect("/signin")
+  get "wp-login.php?action=logout&_wpnonce=:nonce", to: redirect("/signout")
 
   # Redirects
   get "store", to: redirect("http://store.crimethinc.com")
