c28ecd06b849d75ade88485dc3b37a141ddc59f3
Bookis Smuin <bookis.smuin@gmail.com>
2016-12-29 08:39:31 -0800

    Remove unused (and possibly dangerous) password column on users table

--- db/migrate/20160829224749_create_users.rb
+++ db/migrate/20160829224749_create_users.rb
@@ -1,7 +1,7 @@
 class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
-      t.string :username, :email, :display_name, :password, :password_digest
+      t.string :username, :email, :display_name, :password_digest
       t.text :avatar
 
       t.timestamps
