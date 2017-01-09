9a558eb16ec52c96bf7ff2631e43ed244781ac5f
Shane Becker <veganstraightedge@gmail.com>
2017-01-09 00:05:09 -0800

    Move /work/extras assets to S3

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,2 +1,9 @@
 module BooksHelper
+  def thumbnail_link_to_large_image(small_url, large_url=nil)
+    if large_url.nil?
+      large_url = small_url.sub("small.", "large.")
+    end
+
+    link_to(image_tag(small_url), large_url)
+  end
 end
