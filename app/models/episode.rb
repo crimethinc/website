cdbb81d355252840b15344be616b8e3d2635d8ef
Shane Becker <veganstraightedge@gmail.com>
2017-01-10 16:52:51 -0800

    Fix/improve /admin/episode form views

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -1,6 +1,14 @@
 class Episode < ApplicationRecord
   belongs_to :podcast
 
+  def name
+    if title.present? && subtitle.present?
+      "#{title} : #{subtitle}"
+    else
+      title
+    end
+  end
+
   def path
     "/podcast/#{to_param}"
   end
