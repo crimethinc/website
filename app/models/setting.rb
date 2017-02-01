9eac531291fe5d9428b0ff5ba5b653f1f896a64d
Shane Becker <veganstraightedge@gmail.com>
2017-02-01 10:16:19 -0800

    Remove slugging on Setting

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -1,13 +1,7 @@
 class Setting < ApplicationRecord
-  include Slug
-
   validates :name, presence: true, uniqueness: true
 
   def content
     saved_content.blank? ? fallback : saved_content
-  end
-
-  def generate_slug
-    self.slug = super.gsub("-", "_")
   end
 end
