364619a46928390b816b2e8c5534ff51d761cd19
Shane Becker <veganstraightedge@gmail.com>
2017-02-03 21:55:23 -0800

    Remove phone number from /podcast

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,7 @@ ActiveRecord::Schema.define(version: 20170203041801) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "pg_stat_statements"
 
   create_table "articles", force: :cascade do |t|
     t.integer  "user_id"
