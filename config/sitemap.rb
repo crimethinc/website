5cd841132b518d3e10cb0e6b7de6207bb380cfbe
Aki Rose Braun <hi@akiro.se>
2017-02-04 00:44:07 -0500

    Better execution on sitemap generation.

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,7 +1,5 @@
 # Set the host name for URL creation
-SitemapGenerator::Sitemap.default_host = "https://crimethinc.com"
-
-SitemapGenerator::Sitemap.create do
+SitemapGenerator::Sitemap.create(default_host: "https://crimethinc.com", compress: true)  do
   # Put links creation logic here.
   #
   # The root path '/' and sitemap index file are added automatically for you.
@@ -19,25 +17,47 @@ SitemapGenerator::Sitemap.create do
   #
   # add article_path, :priority => 0.7, :changefreq => 'daily'
 
-  add_to_index '/feed'
-  add_to_index '/podcast/feed'
-
   static_paths = [
+      "/about/",
+      "/arts/submission-guidelines",
+      "/books/into-libraries/", "/books/lit-kit/",
+      "/categories/",
+      "/get/",
+      "/kickstarter/2017/",
+      "/listen/",
+      "/read/",
       "/rt/", "/rt/archives/",
+      "/start/",
       "/store/", "/store/audio/", "/store/added/",
-      "/kickstarter/2017/"
+      "/tce/",
+      "/watch/",
   ]
-
   tce_languages = ["czech", "deutsch", "espanol", "polski", "portugues", "quebecois", "slovenscina", "slovensko"]
+
   tce_languages.each do |lang|
     static_paths.push("/tce/#{lang}/", "/tce/#{lang}/get/")
   end
 
   static_paths.each do |path|
-    add path, changefreq: "monthly", lastmod: "2016-01-01"
+    add path, changefreq: "monthly", lastmod: "2017-01-01"
   end
 
-  [Article, Book, Episode, Page, Podcast, Video].each do |model|
+  add_to_index "/feed/", changefreq: "daily", lastmod: Time.now
+  add_to_index "/podcast/feed/", changefreq: "weekly", lastmod: Time.now
+
+  Category.find_each do |category|
+    add_to_index "/categories/#{category.slug}/feed/"
+  end
+
+  Article.live.published.find_each do |page|
+    add page.path, lastmod: page.updated_at
+  end
+
+  Category.find_each do |category|
+    add "/categories/#{category.slug}/"
+  end
+
+  [Book, Episode, Page, Podcast, Video].each do |model|
     model.find_each do |page|
       add page.path, lastmod: page.updated_at
     end
