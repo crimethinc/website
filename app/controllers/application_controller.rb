c65686b34689f2f72d0867415cd74eacc5a53ab2
Shane Becker <veganstraightedge@gmail.com>
2017-02-12 02:13:12 -0800

    Make embed regex stop at whitespace

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -127,7 +127,7 @@ class ApplicationController < ActionController::Base
   helper_method :render_content
 
   def expanded_embeds(post)
-    embed_regex = /\[\[(.*)\]\]/
+    embed_regex = /\[\[(\S*)\]\]/
 
     output_content = post.content.gsub(embed_regex) do |match|
       embed_tag = $1
@@ -148,6 +148,8 @@ class ApplicationController < ActionController::Base
   helper_method :expanded_embeds
 
   def expanded_embed(url, caption=nil)
+    url = url.sub("]]", "").sub("[[", "")
+
     url  = URI.parse(url)
 
     case url.host
