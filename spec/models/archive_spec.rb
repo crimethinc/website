f02db341e11bd988e4a2905f54737014fc205959
greggawatt <greggahorton@gmail.com>
2017-02-28 20:46:36 -0800

    byebug:

--- spec/models/archive_spec.rb
+++ spec/models/archive_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Archive, type: :model do
     let(:status)  { Status.new(name: "published") }
     let!(:first) { create(:article, published_at: DateTime.parse("2017-01-01"), status: status, short_path: SecureRandom.hex) }
     let!(:last) { create(:article, published_at: DateTime.parse("2017-01-20"), status: status, short_path: SecureRandom.hex) }
-    byebug
+
     subject { Archive.new(year: "2017", month: "01").first }
 
     it { is_expected.to eq([2017, { 1 => [last, first] }]) }
