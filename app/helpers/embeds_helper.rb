fa6ad12f534eacefcf9f95976290aacaaef2a44a
Shane Becker <veganstraightedge@gmail.com>
2017-05-06 21:15:20 -0700

    Embed tweet without hitting the network

--- app/helpers/embeds_helper.rb
+++ app/helpers/embeds_helper.rb
@@ -1,15 +1,4 @@
 module EmbedsHelper
-  def embed_twitter(url)
-    begin
-      oembed_url = open("https://publish.twitter.com/oembed?url=#{url}")
-    rescue OpenURI::HTTPError => e
-      return "#{embed_link(url)} (deleted)".html_safe
-    end
-
-    oembed_json = oembed_url.read
-    JSON.parse(oembed_json)["html"].html_safe
-  end
-
   def embed_link(url, id: nil)
     link_to(url.to_s.sub(/^https*:\/\//, "").sub(/\/$/, ""), url.to_s, id: id).html_safe
   end
