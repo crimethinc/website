ce615eb525dd8f2a0d4e81698ccc41bee5bca4b2
Matt Pruitt <guitsaru@gmail.com>
2017-05-11 21:48:01 -0700

    Fix asset pipeline deprecation warning in specs

--- spec/helpers/books_helper_spec.rb
+++ spec/helpers/books_helper_spec.rb
@@ -3,15 +3,15 @@ require "rails_helper"
 RSpec.describe BooksHelper, type: :helper do
   describe "#thumbnail_link_to_large_image" do
     context "with large_url" do
-      subject { helper.thumbnail_link_to_large_image("test/small.png", "/test/larger.png") }
+      subject { helper.thumbnail_link_to_large_image("http://example.com/test/small.png", "/test/larger.png") }
 
-      it { is_expected.to eq(%{<a href="/test/larger.png"><img src="/images/test/small.png" alt="Small" /></a>}) }
+      it { is_expected.to eq(%{<a href="/test/larger.png"><img src="http://example.com/test/small.png" alt="Small" /></a>}) }
     end
 
     context "without large_url" do
-      subject { helper.thumbnail_link_to_large_image("test/small.png") }
+      subject { helper.thumbnail_link_to_large_image("http://example.com/test/small.png") }
 
-      it { is_expected.to eq(%{<a href="test/large.png"><img src="/images/test/small.png" alt="Small" /></a>}) }
+      it { is_expected.to eq(%{<a href="http://example.com/test/large.png"><img src="http://example.com/test/small.png" alt="Small" /></a>}) }
     end
   end
 end
