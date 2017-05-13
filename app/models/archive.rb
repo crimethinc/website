239e4541faa355ad418f75f0edd87b71416dda6e
Matt Pruitt <guitsaru@gmail.com>
2017-05-12 19:13:09 -0700

    Fix duplicate articles in archive.
    During the initial article import, many of the articles had the same
    created_at.  It looks like the sort wasn't deterministic when only
    sorting by created_at causing some articles to be duplicated when
    paginating.  This commit adds a secondary sort by article title so that
    it will always sort them in the same way.
    
    Closes #359

--- app/models/archive.rb
+++ app/models/archive.rb
@@ -10,12 +10,13 @@ class Archive
     @year  = year
     @month = month
     @page  = page
+    @day   = day
   end
 
   def articles
     return @articles if defined?(@articles)
 
-    @articles = Article.published.live.root.all
+    @articles = Article.published.live.root.order(title: :desc)
 
     @articles = @articles.where(year:  year)  if year.present?
     @articles = @articles.where(month: month) if month.present?
