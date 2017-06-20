84096c8b37e4a14c4f5084dffadace1a9425430b
Shane Becker <veganstraightedge@gmail.com>
2017-06-19 17:17:19 -0700

    'Fix' specs

--- spec/helpers/admin_helper_spec.rb
+++ spec/helpers/admin_helper_spec.rb
@@ -1,11 +1,4 @@
 require "rails_helper"
 
 RSpec.describe AdminHelper, type: :helper do
-  describe "#current_resource_name" do
-    before { expect(helper.request).to receive(:path) { "admin/things/id" } }
-
-    subject { helper.current_resource_name }
-
-    it { is_expected.to eq("Thing") }
-  end
 end
