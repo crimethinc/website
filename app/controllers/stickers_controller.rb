eae35bdaa8d8ebae6f21ea07edd540faaadfd0dc
Shane Becker <veganstraightedge@gmail.com>
2017-06-30 15:18:58 -0700

    Fix @type for stickers [update #128]

--- app/controllers/stickers_controller.rb
+++ app/controllers/stickers_controller.rb
@@ -2,7 +2,7 @@ class StickersController < ApplicationController
   def index
     @html_id  = "page"
     @body_id  = "products"
-    @type     = "sticker"
+    @type     = "stickers"
     @products = Poster.sticker.all
 
     render "products/index"
