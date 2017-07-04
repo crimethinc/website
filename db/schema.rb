55ca445f8f9cde117934f2ebb2d0721819e9c4f4
astronaut-wannabe <contact@astronaut-wannabe.com>
2017-07-04 15:59:05 +0200

    fix db/schema so CI passes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170624193008) do
+ActiveRecord::Schema.define(version: 20170704083543) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
