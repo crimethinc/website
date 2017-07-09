991242cd70ceab0a7bf2417d6243935ed0362971
greggawatt <greggahorton@gmail.com>
2017-07-09 10:19:10 -0700

    fixing schema again

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,7 @@ ActiveRecord::Schema.define(version: 20170624193008) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "pg_stat_statements"
 
   create_table "articles", id: :serial, force: :cascade do |t|
     t.integer "user_id"
