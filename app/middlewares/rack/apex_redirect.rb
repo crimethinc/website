cee701a346e0a427f902ab55e7e8563932dee7b5
Shane Becker <veganstraightedge@gmail.com>
2017-07-24 16:10:36 -0700

    Use rack to strip www. [update  #375]

--- app/middlewares/rack/apex_redirect.rb
+++ app/middlewares/rack/apex_redirect.rb
@@ -8,7 +8,7 @@ module Rack
       request = Rack::Request.new(env)
 
       if request.host.start_with?("www.")
-        location = request.scheme + request.host.sub("www.", "") + request.path
+        location = request.scheme + "://" + request.host.sub("www.", "") + request.path
         return redirect(location)
       end
 
