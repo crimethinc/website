5f487fb3f279dea8daf2d5eec63af01035e9f1cc
Shane Becker <veganstraightedge@gmail.com>
2017-08-11 13:35:33 -0700

    Update /admin/links styles

--- app/controllers/admin/links_controller.rb
+++ app/controllers/admin/links_controller.rb
@@ -2,24 +2,20 @@ class Admin::LinksController < Admin::AdminController
   before_action :authorize
   before_action :set_link, only: [:show, :edit, :update, :destroy]
 
-  # /admin/links
   def index
     @links = Link.page(params[:page])
     @title = admin_title
   end
 
-  # /admin/links/1
   def show
-    @title = admin_title(@link, [:name])
+    redirect_to [:admin, :links]
   end
 
-  # /admin/links/new
   def new
     @link = Link.new
     @title = admin_title
   end
 
-  # /admin/links/new
   def create
     @link = Link.new(link_params)
 
@@ -30,12 +26,10 @@ class Admin::LinksController < Admin::AdminController
     end
   end
 
-  # /admin/links/edit/1
   def edit
     @title = admin_title(@link, [:id, :name])
   end
 
-  # /admin/links/edit/1
   def update
     if @link.update(link_params)
       redirect_to [:admin, :links], notice: "Link was successfully updated."
