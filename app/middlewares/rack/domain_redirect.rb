6c93de0898f27ec5d957a713c0df9afdb8ff9227
Shane Becker <veganstraightedge@gmail.com>
2017-08-30 01:04:36 -0700

    Add cwc.im => crimethinc.com rack redirect

---
+++ app/middlewares/rack/domain_redirect.rb
@@ -0,0 +1,28 @@
+module Rack
+  class DomainRedirect
+    def initialize(app)
+      @app = app
+    end
+
+    def call(env)
+      request = Rack::Request.new(env)
+
+      if request.host.downcase =~ /cwc/
+        location = "https://crimethinc.com" + request.path
+        return redirect(location)
+      end
+
+      @app.call(env)
+    end
+
+    private
+
+    def redirect(location)
+      [
+        301,
+        { "Location" => location, "Content-Type" => "text/html" },
+        ["Moved Permanently"]
+      ]
+    end
+  end
+end
--- config/application.rb
+++ config/application.rb
@@ -1,9 +1,10 @@
 require_relative "boot"
 
 require "rails/all"
-require_relative "../app/middlewares/rack/redirect"
+require_relative "../app/middlewares/rack/domain_redirect"
 require_relative "../app/middlewares/rack/apex_redirect"
 require_relative "../app/middlewares/rack/blog_redirect"
+require_relative "../app/middlewares/rack/redirect"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -14,9 +15,10 @@ module Crimethinc
     # Initialize configuration defaults for originally generated Rails version.
     # config.load_defaults 5.1
 
-    config.middleware.use Rack::Redirect
+    config.middleware.use Rack::DomainRedirect
     config.middleware.use Rack::ApexRedirect
     config.middleware.use Rack::BlogRedirect
+    config.middleware.use Rack::Redirect
     config.middleware.use Rack::Attack
   end
 end
