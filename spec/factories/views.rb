6d2438e9693299f7388ff5c8949ac12e48cb0226
Shane Becker <veganstraightedge@gmail.com>
2017-12-12 22:54:05 -1000

    Drop View model (previously for article page view counts)

--- spec/factories/views.rb
+++
@@ -1,5 +0,0 @@
-FactoryBot.define do
-  factory :view do
-    article nil
-  end
-end
--- spec/models/view_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe View, type: :model do
-  # pending "add some examples to (or delete) #{__FILE__}"
-end
