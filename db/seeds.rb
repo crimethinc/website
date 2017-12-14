a501da01c7b5c3c75ce037d54a112c77f3035cfd
Shane Becker <veganstraightedge@gmail.com>
2017-12-14 13:15:56 -1000

    Create test user in seeds for pr apps

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,13 +2,11 @@
   Status.create!(name: status)
 end
 
-if Rails.env.development?
-  test_user = User.new(
-                username:              "tester",
-                password:              "a long passphrase to meet the minimum length",
-                password_confirmation: "a long passphrase to meet the minimum length")
-  test_user.save!(validate: false)
-end
+test_user = User.new(
+              username:              "tester",
+              password:              "a long passphrase to meet the minimum length",
+              password_confirmation: "a long passphrase to meet the minimum length")
+test_user.save!(validate: false)
 
 puts "Trying dev seeds for each post-type..."
 %w(articles books pages links podcasts episodes redirects videos).each do |posttype|
