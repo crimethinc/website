704b64628631e4faa71a916931eeda0071efd188
Shane Becker <veganstraightedge@gmail.com>
2017-12-14 21:00:42 -1000

    Add stub donation page

---
+++ app/controllers/donate_controller.rb
@@ -0,0 +1,9 @@
+class DonateController < ApplicationController
+  def index
+    @html_id = "page"
+    @body_id = "donate"
+    @title   = "Invest Your Money in CrimethInc."
+
+    redirect_to [:root] unless staging? or Rails.env.development?
+  end
+end
