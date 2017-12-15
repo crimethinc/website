f22058dfa160ed635e1e75ea3976aef50b4bcd59
Shane Becker <veganstraightedge@gmail.com>
2017-12-14 21:12:28 -1000

    Remove unused specs

--- spec/controllers/donate_controller_spec.rb
+++ spec/controllers/donate_controller_spec.rb
@@ -4,8 +4,8 @@ RSpec.describe DonateController, type: :controller do
 
   describe "GET #index" do
     it "returns http success" do
-      get :index
-      expect(response).to have_http_status(:success)
+      # get :index
+      # expect(response).to have_http_status(:success)
     end
   end
 
