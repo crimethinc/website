1098faaa08c4134f04618190674e90b10f31e29c
Shane Becker <veganstraightedge@gmail.com>
2017-12-29 00:47:51 -1000

    Move social links from db lookup to static strings in view helper

--- spec/helpers/links_helper_spec.rb
+++ spec/helpers/links_helper_spec.rb
@@ -2,9 +2,7 @@ require "rails_helper"
 
 RSpec.describe LinksHelper, type: :helper do
   describe "#social_link_classes" do
-    let(:link) { Link.new(url: "http://example.com", name: "example") }
-
-    subject { helper.social_link_classes(link) }
+    subject { helper.social_link_classes(url: "http://example.com", name: "example") }
 
     it { is_expected.to eq("link-name-example link-domain-example") }
   end
