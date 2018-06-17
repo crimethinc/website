69e1dfcd170c85b6c5e77ea08fa687f3737257e4
Shane Becker <veganstraightedge@gmail.com>
2018-06-16 22:58:52 -0700

    [Corrected] Style/TrailingCommaInHashLiteral: Avoid comma after the last item of a hash.

--- app/controllers/admin/books_controller.rb
+++ app/controllers/admin/books_controller.rb
@@ -73,7 +73,7 @@ module Admin
         print_black_and_white_a4: ['Print B/W A4',            'Is there an A4 sized B/W <code>PDF</code> for printing uploaded?'],
         epub:                     ['ePub',                    'Is there a <code>.epub</code> file uploaded?'],
         mobi:                     ['Mobi',                    'Is there a <code>.mobi</code> file uploaded?'],
-        lite:                     ['Lo Res',                  'Is there a low resolution or single page view PDF uploaded?'],
+        lite:                     ['Lo Res',                  'Is there a low resolution or single page view PDF uploaded?']
       }
     end
 
