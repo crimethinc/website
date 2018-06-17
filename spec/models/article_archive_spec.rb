5d3e4cb90dd515181b00a2f6d8186d9c1f642854
Shane Becker <veganstraightedge@gmail.com>
2018-06-16 20:23:11 -0700

    Reduce Rubocop warnings: RSpec/LeadingSubject
    Declare subject above any other let declarations.

--- spec/models/article_archive_spec.rb
+++ spec/models/article_archive_spec.rb
@@ -2,12 +2,12 @@ require 'rails_helper'
 
 RSpec.describe ArticleArchive, type: :model do
   describe 'it sorts the articles correctly' do
+    subject { ArticleArchive.new(year: '2017', month: '01').first }
+
     Status.delete_all
     let(:status) { create(:status, :published) }
     let!(:first) { create(:article, published_at: Date.parse('2017-01-01'), status: status) }
     let!(:last) { create(:article, published_at: Date.parse('2017-01-20'), status: status) }
-
-    subject { ArticleArchive.new(year: '2017', month: '01').first }
 
     it { is_expected.to eq([2017, { 1 => [last, first] }]) }
   end
