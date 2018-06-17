7196f4b8f36124f4357da8cd18dda0952219ad72
Shane Becker <veganstraightedge@gmail.com>
2018-06-16 21:56:52 -0700

    [Corrected] Style/Lambda
    Use the -> { ... } lambda literal syntax for single line lambdas.

--- app/models/concerns/publishable.rb
+++ app/models/concerns/publishable.rb
@@ -11,9 +11,9 @@ module Publishable
     scope :root,          -> { where(collection_id: nil) }
     scope :live,          -> { where('published_at < ?', Time.now) }
     scope :recent,        -> { where('published_at BETWEEN ? AND ?', Time.now - 2.days, Time.now) }
-    scope :on,            lambda { |date| where('published_at BETWEEN ? AND ?', date.try(:beginning_of_day), date.try(:end_of_day)) }
-    scope :next,          lambda { |article| unscoped.root.where('published_at > ?', article.published_at).live.published.order(published_at: :asc).limit(1) }
-    scope :previous,      lambda { |article| root.where('published_at < ?', article.published_at).live.published.chronological.limit(1) }
+    scope :on,            ->(date) { where('published_at BETWEEN ? AND ?', date.try(:beginning_of_day), date.try(:end_of_day)) }
+    scope :next,          ->(article) { unscoped.root.where('published_at > ?', article.published_at).live.published.order(published_at: :asc).limit(1) }
+    scope :previous,      ->(article) { root.where('published_at < ?', article.published_at).live.published.chronological.limit(1) }
   end
 
   def draft?
