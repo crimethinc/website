c02ee56a3395b2653b53c6893d210a6c43165c23
Eli Duke <elimduke@gmail.com>
2018-09-18 12:49:42 -0700

    Adds sendgrid config to environment.rb for initial testing purposes

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  user_name: ENV['SENDGRID_USERNAME'],
+  password: ENV['SENDGRID_PASSWORD'],
+  domain: 'crimethinc.com',
+  address: 'smtp.sendgrid.net',
+  port: 587,
+  authentication: :plain,
+  enable_starttls_auto: true
+}
