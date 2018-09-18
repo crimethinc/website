f0630b10fd0f46060a5ea5d566cceda76b940d10
Eli Duke <elimduke@gmail.com>
2018-09-18 11:51:18 -0700

    Localization for Support views: new and edit

--- app/helpers/support_helper.rb
+++ app/helpers/support_helper.rb
@@ -1,11 +1,5 @@
 module SupportHelper
-  def time_until_expiration(support_session)
-    minutes = (support_session.expires_at.to_i - Time.current.to_i) / 60
-
-    if minutes.zero?
-      'less than a minute'
-    else
-      'about ' + pluralize(minutes, 'minute')
-    end
+  def minutes_until_expiration(support_session)
+    (support_session.expires_at.to_i - Time.current.to_i) / 60
   end
 end
