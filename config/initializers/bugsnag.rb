d8db11ac8c4f8c16131b65a4d588ced851f79f93
astronaut-wannabe <contact@astronaut-wannabe.com>
2018-09-24 16:19:35 -0700

    Make bugsnag differentiate staging and production

--- config/initializers/bugsnag.rb
+++ config/initializers/bugsnag.rb
@@ -1,4 +1,14 @@
 Bugsnag.configure do |config|
   config.api_key = '52ca1da5064c3aa7d8bdf7cf4f35f2c3'
   config.notify_release_stages = %w[staging production]
+
+  # we consider both staging and production as 'production' for heroku
+  # reasons, so set based on ENV variables
+  if Rails.env.production?
+    staging = ENV['ON_STAGING'] == 'TRUE'
+    config.release_stage = staging ? 'staging' : 'production'
+  else
+    # otherwise it is 'test' or 'development'
+    config.release_stage = Rails.env
+  end
 end
