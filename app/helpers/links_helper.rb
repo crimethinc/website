c46e0169664626fa85749704e6698f4200b451a7
Shane Becker <veganstraightedge@gmail.com>
2018-09-25 22:55:19 -0700

    Remove some (mostly) unused social links

--- app/helpers/links_helper.rb
+++ app/helpers/links_helper.rb
@@ -4,13 +4,6 @@ module LinksHelper
       'CrimethInc. on Twitter'        => 'https://twitter.com/crimethinc',
       'CrimethincDotCom on Facebook'  => 'https://facebook.com/CrimethincDotCom',
       'CrimethincDotCom on Instagram' => 'https://instagram.com/CrimethincDotCom',
-      'Crimethinc. on Medium'         => 'https://medium.com/@crimethinc',
-      'CrimethInc. on Tumblr'         => 'http://crimethincdotcom.tumblr.com',
-      'CrimethInc. on Wordpress'      => 'https://crimethincdotcom.wordpress.com',
-      'CrimethInc. on Vimeo'          => 'https://vimeo.com/crimethinc',
-      'CrimethInc. on Youtube'        => 'https://www.youtube.com/channel/UCH9VbQUhFVjTyUZhZqDVlDw',
-      'Crimethinc. on Periscope'      => 'https://www.periscope.tv/crimethinc',
-      'CrimethInc. on Snapchat'       => 'https://www.snapchat.com/add/crimethinc',
       'CrimethInc. on Patreon'        => 'https://www.patreon.com/crimethinc',
       'CrimethInc.com RSS feed'       => 'https://crimethinc.com/feed',
       'CrimethInc.com on Github'      => 'https://github.com/crimethinc'
