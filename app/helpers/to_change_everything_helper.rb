262693ffbd4d95bf67ce497e7d61ac03025e139a
Shane Becker <veganstraightedge@gmail.com>
2018-10-21 22:14:17 -0700

    DRY refactor TCE

--- app/helpers/to_change_everything_helper.rb
+++ app/helpers/to_change_everything_helper.rb
@@ -1,5 +1,12 @@
 module ToChangeEverythingHelper
   TCE_IMAGE_BASE_URL = 'https://cloudfront.crimethinc.com/assets/tce/images/'.freeze
+  TCE_SECTIONS       = %w[self answering power relationships reconciling liberation revolt
+                          control hierarchy borders representation leaders government profit
+                          property lastcrime].freeze
+
+  def tce_sections
+    TCE_SECTIONS
+  end
 
   def url_for_tce_image *pieces
     [TCE_IMAGE_BASE_URL, pieces].flatten.join
