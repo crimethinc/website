3a7f03c8ea4f125370ac054480eaaf86565ff1fe
Shane Becker <veganstraightedge@gmail.com>
2018-10-27 20:53:15 -0700

    Return seeds.rb back to normal

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,15 @@
-# %w(draft published).each do |status|
-#   Status.create!(name: status)
-# end
-#
-# test_user = User.new(
-#               username:              "tester",
-#               password:              "a long passphrase to meet the minimum length",
-#               password_confirmation: "a long passphrase to meet the minimum length")
-# test_user.save!(validate: false)
+%w(draft published).each do |status|
+  Status.create!(name: status)
+end
+
+test_user = User.new(
+              username:              "tester",
+              password:              "a long passphrase to meet the minimum length",
+              password_confirmation: "a long passphrase to meet the minimum length")
+test_user.save!(validate: false)
 
 puts "Trying dev seeds for each post-type..."
-# %w[articles books pages podcasts episodes redirects videos journals featured_journals].each do |posttype|
-%w[featured_journals].each do |posttype|
+%w[articles books pages podcasts episodes redirects videos journals featured_journals].each do |posttype|
   filepath = File.expand_path("../seeds/#{posttype}.rb", __FILE__)
 
   puts "  Trying: #{posttype}"
