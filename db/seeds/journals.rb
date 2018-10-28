1f7c79be32a3fcc1013d2a7dae8f5908b0ebf5dd
Shane Becker <veganstraightedge@gmail.com>
2018-10-27 18:32:14 -0700

    Fix RT TOC PDF URLs in seeds/journals

--- db/seeds/journals.rb
+++ db/seeds/journals.rb
@@ -15,7 +15,7 @@ summary: %q{
 Rolling Thunder #10 begins with a reappraisal of the anarchist project in today’s context of crisis and technological transformation. From there, we chart the global trajectory of momentum from 2010 to 2012: the student movements in the [US](/2010/03/09/march-4-anarchists-in-the-student-movement) and [UK](/2011/01/27/the-uk-student-movement)—the insurrections in Tunisia, [Egypt](/2011/02/02/egypt-today-tomorrow-the-world), and beyond—the occupation movements in [Spain](/2011/06/08/fire-extinguishers-and-fire-starters-anarchist-interventions-in-the-spanish-revolution-an-account-from-barcelona), Greece, and finally the USA, from its awkward beginnings in [Wisconsin](/2011/03/10/spread-the-chaos-from-capitol-to-capital) to its aftereffects in [Oakland](/2012/05/10/may-day-a-strike-is-a-blow). For case studies, we focus in on the anti-police struggles that catalyzed the rise of confrontational anarchism in Seattle, and scrutinize how US immigration policy is applied on the ground at the border to explain how its actual objectives differ from its ostensible purpose. The issue concludes with a historical review of Canadian anarchism, following it from its origins through the 2010 [Olympics](/2010/02/16/riot-2010) and [G20](/2010/07/06/toronto-g20-eyewitness-report) riots and up to the present day. All this, plus a graphic history from Argentine anarchism, 24 pages in full color, and all the other bells and whistles you’ve come to expect from us times two.
 },
 description: %q{
-[Download Table of Contents PDF](https://cloudfront.crimethinc.com/assets/journals/rolling-thunder-10/rolling-thunder-10_table_of_contents.pdf) (140 k).
+[Download Table of Contents PDF](https://cloudfront.crimethinc.com/assets/journals/rolling-thunder-10-summer-2012/rolling-thunder-10-summer-2012_table_of_contents.pdf) (140 k).
 
 Full issue PDF download: 26 MB.
 
@@ -35,7 +35,7 @@ summary: %q{
 How important is legitimacy—in our own eyes, in the eyes of potential allies, in the eyes of the public? How can anarchists cultivate it? What pitfalls does it hold? _Rolling Thunder_ #9 explores these questions while reporting on the past six months of upheavals around the US. Following up on our coverage of the [2008 convention protests](/2009/05/05/going-it-alone), this issue assesses anarchist action at the 2009 [G20 summit](/2009/10/07/g20-mobilization-preliminary-assessment), mapping conflict throughout the city and analyzing the strategies of the police and protesters. The accompanying Pittsburgh scene report examines the decade of local organizing that prepared the ground for this and other confrontations. Elsewhere herein, we scrutinize protest and resistance on campus—from the recent [occupation movement](http://afterthefallcommuniques.info) to efforts to shut down fascist student organizations—and overseas in the [Smash EDO](http://www.smashedo.org.uk) campaign. All this, plus obscure Russian history, a reappraisal of the concept of “free speech,” and the usual stunning prose. No advertisements; 16 full-color pages. Check out the [online supplement](/2010/03/03/rolling-thunder-9) to this issue.
 },
 description: %q{
-[Download Table of Contents PDF](https://cloudfront.crimethinc.com/assets/journals/rolling-thunder-9/rolling-thunder-9_table_of_contents.pdf) (303 k).
+[Download Table of Contents PDF](https://cloudfront.crimethinc.com/assets/journals/rolling-thunder-9-spring-2010/rolling-thunder-9-spring-2010_table_of_contents.pdf) (303 k).
 
 Full issue PDF download: 22 MB.
 
@@ -52,7 +52,7 @@ summary: %q{
 So much happened in the second half of 2008 that we had to put off all our other content and add an extra 8 pages just to cover it all. Anarchists coordinated mass mobilizations against the Democratic and Republican National Conventions, provoking major clashes; the global economy collapsed; Greece experienced an anarchist-organized insurrection in response to a police murder; and at the beginning of 2009, Oakland was shaken by similar unrest. Our coverage pushes beyond the surface of events to offer insight into the organizing structures and historical background, fleshing out timelines and analyses with personal narratives and cutting-edge cartography. In addition to all this, the issue includes an exploration of the relationship between the punk subculture and the anarchist movement, complemented by interviews with bands and collectives from beyond the white punk ghetto, and ends with a primer on small-town organizing using Winona, Minnesota as a case study. No advertisements; 24 pages in full color.
 },
 description: %q{
-[Download Table of Contents PDF](https://cloudfront.crimethinc.com/assets/journals/rolling-thunder-7/rolling-thunder-7_table_of_contents.pdf) (512 k).
+[Download Table of Contents PDF](https://cloudfront.crimethinc.com/assets/journals/rolling-thunder-7-spring-2009/rolling-thunder-7-spring-2009_table_of_contents.pdf) (512 k).
 
 Full issue PDF download: 13 MB.
 
@@ -69,7 +69,7 @@ summary: %q{
 The theme of _Rolling Thunder_ #6 is experimentation: the processes by which radicals invent and refine new approaches. To this end, it features an evaluation of the model [activists](http://en.wikipedia.org/wiki/Stop_Huntingdon_Animal_Cruelty) have used to target the animal testing corporation HLS, discussing whether it could be effective in other contexts; a photoessay documenting the efforts of [Swedish anarchists](http://www.kulturkampanjen.se) who, unable to defend a squat, built a social center from the ground up; a consideration of the role proper support plays in cultivating communities of resistance; a report from student strikes and riots in [Colombia](/texts/recentfeatures/colombia.php); and an analysis of the past decade of anarchist organizing in NYC. In addition, the issue includes an investigation of the function of gift shops in maintaining global empire, historical accounts of Bakunin’s daring escape from Sibera and the riots that killed off the hated poll tax in Britain, and lots more. As usual, there are 16 pages of full color, plenty of fun tidbits, and no advertisements.
 },
 description: %q{
-[Download Table of Contents PDF](https://cloudfront.crimethinc.com/assets/journals/rolling-thunder-6/rolling-thunder-6_table_of_contents.pdf) (186 k).
+[Download Table of Contents PDF](https://cloudfront.crimethinc.com/assets/journals/rolling-thunder-6-fall-2008/rolling-thunder-6-fall-2008_table_of_contents.pdf) (186 k).
 
 Full issue PDF download: 13 MB.
 
@@ -86,7 +86,7 @@ summary: %q{
 This issue focuses on different ways of conceptualizing strategy, exploring the ways anarchist efforts can be repressed, assimilated, and neutralized only to reappear in new forms. It opens with a study by [David Graeber](http://en.wikipedia.org/wiki/David_Graeber) of the successes and stumbling blocks of direct action movements over the past thirty years, followed by a special report distilling lessons from the recent wave of federal repression known as the [Green Scare](/2008/02/22/green-scared). Two features give the inside story on anarchist mobilizations overseas via interviews, personal narratives, and 16 pages of full color photos: an examination of [the riots](https://youtube.com/watch?v=iupbxsHGlAA) following the eviction of Denmark’s beloved social center [Ungdomshuset](http://en.wikipedia.org/wiki/Ungdomshuset), and a full review of last summer’s [G8 protests](http://www.infoshop.org/inews/article.php?story=20070612082401642&query=g8) in Germany. The issue is rounded out by a subject’s analysis of the medical study industry as a case study in modern day precarious labor, a spotlight on anarchist organizing in [Modesto, California](http://www.geocities.com/anarcho209), and reviews of controversial works by anti-art duo [Brener](https://youtube.com/watch?v=P8MM_cf2e-U) and [Schurz](http://en.wikipedia.org/wiki/Barbara_Schurz). As always, 100% ad-free.
 },
 description: %q{
-[Download Table of Contents PDF](https://cloudfront.crimethinc.com/assets/journals/rolling-thunder-5/rolling-thunder-5_table_of_contents.pdf) (510 k).
+[Download Table of Contents PDF](https://cloudfront.crimethinc.com/assets/journals/rolling-thunder-5-spring-2008/rolling-thunder-5-spring-2008_table_of_contents.pdf) (510 k).
 
 Full issue PDF download: 12 MB.
 
@@ -103,7 +103,7 @@ summary: %q{
 The centerpiece of the fourth _Rolling Thunder_ is a full-color photoessay chronicling the popular uprising during which the people of Oaxaca, Mexico wrested control of their city from the government for a period of months. Continuing that theme, other feature articles cover the defense and eviction of South Central Farm in Los Angeles, the Really Really Free Market as a model for reclaiming public space from capitalism and bureaucracy, the resurgence of squatting in Buffalo of all places, the university occupation movement in France, and the ins and outs of urban exploration. The remainder of the issue includes a comprehensive guide to supporting prisoners and defendants, the lyrics to “The Big Rock Candy Mountain” as interpreted by acclaimed comic artist Nate Powell, a gallery of ready-to-use stencils, and plenty of the edgy artwork and poignant prose you’ve come to expect. Still 100% ad-free.
 },
 description: %q{
-[Download Table of Contents PDF](https://cloudfront.crimethinc.com/assets/journals/rolling-thunder-4/rolling-thunder-4_table_of_contents.pdf) (488 k).
+[Download Table of Contents PDF](https://cloudfront.crimethinc.com/assets/journals/rolling-thunder-4-spring-2007/rolling-thunder-4-spring-2007_table_of_contents.pdf) (488 k).
 
 Full issue PDF download: 10 MB.
 
@@ -120,7 +120,7 @@ summary: %q{
 International reports from last Mayday’s pro-immigrant rallies! Analysis of the Bush regime’s strategy to promote terrorism worldwide! Discussion of the latest wave of federal repression! Testimony from a convicted anti-war arsonist! Anarchist perspectives on and reports from the struggle against domestic violence! A tell-all interview with notorious graffiti artist(s) BORF! A shocking exposé on German pro-Zionist lunatics! A how-to guide to funneling resources out of universities! A spy’s-eye-view of immigrant labor in factory farming! A narrow escape from the flaming Pentagon on September 11, 2001! A history of direct action and rioting in queer liberation struggles! A cartoon recounting the riots at the canceled World Bank conference in Barcelona! A mad-lib for radicals with poor social skills! A satirical guide to writing reviews! Visionary storytelling, technical advice on computer security, eulogies and poster designs and more, more, more! 100% advertisement free.
 },
 description: %q{
-[Download Table of Contents PDF](https://cloudfront.crimethinc.com/assets/journals/rolling-thunder-3/rolling-thunder-3_table_of_contents.pdf). (228 k)
+[Download Table of Contents PDF](https://cloudfront.crimethinc.com/assets/journals/rolling-thunder-3-summer-2006/rolling-thunder-3-summer-2006_table_of_contents.pdf). (228 k)
 
 Full issue PDF download: 9 MB.
 
@@ -137,7 +137,7 @@ summary: %q{
 The lengthiest articles in our second issue are an analysis of last summer’s protests against the G8 meeting in Scotland, a retrospective on squatting and resistance culture in northern Europe, and an in-depth discussion of subcultural marginality and refusal that is to dropping out what the first issue’s sixteen-page feature article was to protest activism. The last of these is rounded out by an inside report on the workings of current labor unions, a memoir of gender mutiny, and an account of how to establish a squatted community center; other highlights include an intimate reflection on the patterns by which abusive relationships perpetuate themselves and a primer on communications technology for direct action. This issue also features plenty of the unorthodox fare that distinguishes CrimethInc. projects from the sometimes listless work of other radicals: a haunting fairy tale, cutting edge games to play between more serious adventures, a crossword puzzle, and sheet music for a classic blues song about the preponderance of bread products in the dumpsterer’s diet.
 },
 description: %q{
-[Download Table of Contents PDF](https://cloudfront.crimethinc.com/assets/journals/rolling-thunder-2/rolling-thunder-2_table_of_contents.pdf) (500 k).
+[Download Table of Contents PDF](https://cloudfront.crimethinc.com/assets/journals/rolling-thunder-2-winter-2006/rolling-thunder-2-winter-2006_table_of_contents.pdf) (500 k).
 
 Full issue PDF download: 10 MB.
 
@@ -154,7 +154,7 @@ summary: %q{
 The debut issue includes an extensive analysis of the preceding decade of direct action, feature articles on consent in sexual relationships and alternative conceptions of education, and testimonials from maniacs who squatted their own workplaces and set themselves on fire while fighting police… and that’s just the beginning! All told, this is 116 pages of radical commentary, history, poetry, artwork, fiction, parlor games (!), comics (!!), skills (e.g., “how to survive a police raid”), and culinary advice, all with an emphasis on personal narratives and celebrating the ways subversion thrives in the most unlikely places. 85,000 words adorned with tons of photos and illustrations and not a single advertisement, printed on recycled book paper and square-bound with the fanciest cover we've ever produced.
 },
 description: %q{
-[Download Table of Contents PDF](https://cloudfront.crimethinc.com/assets/journals/rolling-thunder-1/rolling-thunder-1_table_of_contents.pdf) (900 k).
+[Download Table of Contents PDF](https://cloudfront.crimethinc.com/assets/journals/rolling-thunder-1-summer-2005/rolling-thunder-1-summer-2005_table_of_contents.pdf) (900 k).
 
 Full issue PDF download: 13 MB.
 
