892e74b75a7b751931ec835e3b85350ea25cf278
Shane Becker <veganstraightedge@gmail.com>
2018-11-02 13:51:16 -0700

    Debiggen Puma workers (for dev) / embiggen WEB_CONCURRENCY in production

--- config/puma.rb
+++ config/puma.rb
@@ -1,4 +1,4 @@
-workers ENV.fetch('WEB_CONCURRENCY')           { 10 }
+workers ENV.fetch('WEB_CONCURRENCY')           { 2 }
 threads_count = ENV.fetch('RAILS_MAX_THREADS') { 5 }
 threads threads_count, threads_count
 
