805bb4fd34695246a95273cbc234371eb68efb1e
Shane Becker <veganstraightedge@gmail.com>
2018-11-08 20:16:05 -0800

    Refactor some noise out of admin/articles table

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -1,4 +1,6 @@
 module AdminHelper
+  VIEW_COUNTER_GENESIS_DATE = '2017-08-23'.freeze
+
   def admin_form_date(post)
     tz = post&.published_at_tz
     return '' if tz.nil? || post.published_at.nil?
@@ -34,5 +36,38 @@ module AdminHelper
     content_tag :li, class: 'nav-item' do
       link_to text, path, class: classes.join(' '), target: target
     end
+  end
+
+  def class_for_article_counter_genesis article
+    # View counts started on 2017-08-23.
+    # Border on this article (2017-08-23) indicates when the view counter started.
+    # Everything below it was published pre-counter.
+
+    return if article.published_at.blank? ||
+              article.published_at.strftime('%Y-%m-%d') != VIEW_COUNTER_GENESIS_DATE
+
+    'article-counter-genesis'
+  end
+
+  def class_for_being_edited article
+    # When an article is being edited by another person, let others know,
+    # so they don’t overwrite each others’ edit.
+
+    return if article.user.blank?
+
+    'bg-warning'
+  end
+
+  def class_for_article_precounter_views article
+    # View counts started on 2017-08-23.
+
+    return if article.published_at.blank? ||
+              article.published_at.strftime('%Y-%m-%d') > VIEW_COUNTER_GENESIS_DATE
+
+    'text-muted'
+  end
+
+  def admin_articles_table_row_classes article
+    [class_for_being_edited(article), class_for_article_counter_genesis(article)].join(' ')
   end
 end
