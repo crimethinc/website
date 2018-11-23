29bc202fcf2c5291f1c4f24e7325d05e2f2d49b6
Shane Becker <veganstraightedge@gmail.com>
2018-11-23 14:35:58 -0800

    Move #render_content to MarkdownHelper

--- app/helpers/markdown_helper.rb
+++ app/helpers/markdown_helper.rb
@@ -15,13 +15,10 @@ module MarkdownHelper
       transliterated_header_ids: true
     ).to_html.html_safe
   end
-<<<<<<< HEAD
 
   def render_content article
     cache [:article_content, article, lite_mode?] do
       article.content_rendered include_media: media_mode?
     end
   end
-=======
->>>>>>> Move #render_markdown to MarkdownHelper
 end
