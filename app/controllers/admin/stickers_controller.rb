b10398bca2b275932970bedf5fcee463885b5f4f
Shane Becker <veganstraightedge@gmail.com>
2018-12-08 11:52:43 -0800

    FIX: Use the Sticker model, not Poster, in admin/stickers

--- app/controllers/admin/stickers_controller.rb
+++ app/controllers/admin/stickers_controller.rb
@@ -15,7 +15,7 @@ module Admin
     end
 
     def new
-      @poster = Poster.new
+      @poster = Sticker.new
       @title  = admin_title
       render 'admin/posters/new'
     end
@@ -26,7 +26,7 @@ module Admin
     end
 
     def create
-      @poster = Poster.new(poster_params)
+      @poster = Sticker.new(poster_params)
 
       if @poster.save
         redirect_to [:admin, @poster], notice: 'Sticker was successfully created.'
@@ -51,15 +51,15 @@ module Admin
     private
 
     def set_poster
-      @poster = Poster.find(params[:id])
+      @poster = Sticker.find(params[:id])
     end
 
     def poster_params
-      params.require(:poster).permit(:sticker, :title, :subtitle, :content, :published_at,
-                                     :content_format, :buy_info, :buy_url, :price_in_cents,
-                                     :summary, :description, :front_image_present,
-                                     :back_image_present, :lite_download_present, :slug, :height,
-                                     :width, :publication_status)
+      params.require(:sticker).permit(:sticker, :title, :subtitle, :content, :published_at,
+                                      :content_format, :buy_info, :buy_url, :price_in_cents,
+                                      :summary, :description, :front_image_present,
+                                      :back_image_present, :lite_download_present, :slug, :height,
+                                      :width, :publication_status)
     end
   end
 end
