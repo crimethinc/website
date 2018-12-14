47e0f37bf5d41e988ce3b901bd124a532e2f637d
astronaut-wannabe <contact@astronaut-wannabe.com>
2018-12-13 16:35:49 -0800

    Fix locale site-switcher
    If you cache on a class method, then it is cached as long as that
    method is in memory

--- app/models/locales.rb
+++ app/models/locales.rb
@@ -3,13 +3,7 @@ class Locales
 
   class << self
     def supported
-      @supported ||= supported_locales
-    end
-
-    private
-
-    def supported_locales
-      @supported = SUPPORTED - [I18n.locale]
+      SUPPORTED - [I18n.locale]
     end
   end
 end
