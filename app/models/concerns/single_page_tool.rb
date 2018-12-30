84cbd38152038eb243dc51d8372f722a2924d8d4
Shane Becker <veganstraightedge@gmail.com>
2018-12-30 02:13:04 -0800

    Extract #front_image and #back_image into smaller methods

--- app/models/concerns/single_page_tool.rb
+++ app/models/concerns/single_page_tool.rb
@@ -27,28 +27,40 @@ module SinglePageTool
     image side: :back, color: :black_and_white
   end
 
-  def image(side: :front, color: :color)
+  def image side: :front, color: :color
     filename = [slug, '_', side, '_', color, '.', send("#{side}_image_format")].join
     [asset_base_url_prefix, filename].join('/')
   end
 
   def front_image
-    if front_color_image_present? || front_black_and_white_image_present?
-      front_color_image_present ? front_color_image : front_black_and_white_image
-    else
-      [asset_base_url_prefix, "#{slug}_front.#{front_image_format}"].join('/')
-    end
+    front_image_present? ? preferred_front_image : fallback_image_url(side: :front)
   end
 
   def back_image
-    if back_color_image_present? || back_black_and_white_image_present?
-      back_color_image_present ? back_color_image : back_black_and_white_image
-    else
-      [asset_base_url_prefix, "#{slug}_back.#{back_image_format}"].join('/')
-    end
+    back_image_present? ? preferred_back_image : fallback_image_url(side: :back)
   end
 
-  def download_url(side: nil, color: nil)
+  def fallback_image_url side:
+    [asset_base_url_prefix, "#{slug}_#{side}.#{send("#{side}_image_format")}"].join('/')
+  end
+
+  def preferred_front_image
+    front_color_image_present ? front_color_image : front_black_and_white_image
+  end
+
+  def preferred_back_image
+    back_color_image_present ? back_color_image : back_black_and_white_image
+  end
+
+  def front_image_present?
+    front_color_image_present? || front_black_and_white_image_present?
+  end
+
+  def back_image_present?
+    back_color_image_present? || back_black_and_white_image_present?
+  end
+
+  def download_url side: nil, color: nil
     filename = [slug]
     filename << "_#{side}"  if side.present?
     filename << "_#{color}" if color.present?
