03f015bd700b121e32fd5617056dcc0a0694edc1
Shane Becker <veganstraightedge@gmail.com>
2018-12-30 00:20:29 -0800

    Extract #back_image #back_image_description #front_image #image_description to MultiPageTool

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,38 +1,6 @@
 class Book < ApplicationRecord
   include MultiPageTool
 
-  def image(side: :front, count: 0)
-    case side
-    when :front
-      [asset_base_url_prefix, "#{slug}_front.jpg"].join('/')
-    when :back
-      [asset_base_url_prefix, "#{slug}_back.jpg"].join('/')
-    when :gallery
-      [asset_base_url_prefix, 'gallery', "#{slug}-#{count}.jpg"].join('/')
-    when :header
-      [asset_base_url_prefix, 'gallery', "#{slug}_header.jpg"].join('/')
-    else
-      [asset_base_url_prefix, 'photo.jpg'].join('/')
-    end
-  end
-
-  def image_description
-    "Photo of ‘#{title}’ front cover"
-  end
-  alias front_image_description image_description
-
-  def back_image_description
-    "Photo of ‘#{title}’ back cover"
-  end
-
-  def front_image
-    image side: :front
-  end
-
-  def back_image
-    image side: :back
-  end
-
   # Overrides
   def ask_for_donation?
     downloads_available?
--- app/models/zine.rb
+++ app/models/zine.rb
@@ -1,35 +1,3 @@
 class Zine < ApplicationRecord
   include MultiPageTool
-
-  def image(side: :front, count: 0)
-    case side
-    when :front
-      [asset_base_url_prefix, "#{slug}_front.jpg"].join('/')
-    when :back
-      [asset_base_url_prefix, "#{slug}_back.jpg"].join('/')
-    when :gallery
-      [asset_base_url_prefix, 'gallery', "#{slug}-#{count}.jpg"].join('/')
-    when :header
-      [asset_base_url_prefix, 'gallery', "#{slug}_header.jpg"].join('/')
-    else
-      [asset_base_url_prefix, 'photo.jpg'].join('/')
-    end
-  end
-
-  def image_description
-    "Photo of ‘#{title}’ front cover"
-  end
-  alias front_image_description image_description
-
-  def back_image_description
-    "Photo of ‘#{title}’ back cover"
-  end
-
-  def front_image
-    image side: :front
-  end
-
-  def back_image
-    image side: :back
-  end
 end
