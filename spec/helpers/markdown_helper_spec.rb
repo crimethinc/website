4d3ab7f58e90390bcf80b827876c9967a1e659c5
Shane Becker <veganstraightedge@gmail.com>
2018-12-30 00:11:26 -0800

    Comment out skipped spec, for now, for quieter output

--- spec/helpers/markdown_helper_spec.rb
+++ spec/helpers/markdown_helper_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe MarkdownHelper, type: :helper do
-  describe '#render_markdown' do
-    # TODO: mock out #media_mode?
-    subject { helper.render_markdown('text').strip }
-
-    # TEMP: un-skip this when above is fixed
-    xit { is_expected.to eq('<p>text</p>') }
-  end
+  # describe '#render_markdown' do
+  #   # TODO: mock out #media_mode?
+  #   subject { helper.render_markdown('text').strip }
+  #
+  #   # TEMP: un-skip this when above is fixed
+  #   xit { is_expected.to eq('<p>text</p>') }
+  # end
 
   describe '#render_content' do
     subject { article.content_rendered.strip }
