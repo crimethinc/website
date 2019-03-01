619570604dc5475f5eda6c806bb8bbbd7c1f103a
Shane Becker <veganstraightedge@gmail.com>
2019-02-28 23:17:36 -0800

    Add locale to admin articles form (#1057)

--- app/controllers/admin/articles_controller.rb
+++ app/controllers/admin/articles_controller.rb
@@ -91,7 +91,7 @@ module Admin
                                       :tweet, :slug, :draft_code, :summary, :published_at,
                                       :tags, :collection_id, :short_path, :image, :css,
                                       :image_description, :image_mobile, :published_at_tz,
-                                      :publication_status, category_ids: [])
+                                      :locale, :publication_status, category_ids: [])
     end
   end
 end
