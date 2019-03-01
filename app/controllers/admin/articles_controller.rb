558e8217305ba542ff2c9d4f2c18ab1e1a90839b
Shane Becker <veganstraightedge@gmail.com>
2019-03-01 00:33:00 -0800

    Article canonical_id to associate translations with English Article (#1058)
    * Add canonical_id to Articles
    
    * Add #canonical and #localizations to Article
    
    * Add form control on admin articles form for canonical_id to associate a translation with an English article

--- app/controllers/admin/articles_controller.rb
+++ app/controllers/admin/articles_controller.rb
@@ -91,7 +91,8 @@ module Admin
                                       :tweet, :slug, :draft_code, :summary, :published_at,
                                       :tags, :collection_id, :short_path, :image, :css,
                                       :image_description, :image_mobile, :published_at_tz,
-                                      :locale, :publication_status, category_ids: [])
+                                      :locale, :canonical_id,
+                                      :publication_status, category_ids: [])
     end
   end
 end
