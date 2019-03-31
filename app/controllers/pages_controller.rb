ae5806ccbbb31c75ea4e924a63c7c39a832a9e90
Shane Becker <veganstraightedge@gmail.com>
2019-03-31 16:15:26 -0700

    Consolidate about controller into pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -11,6 +11,21 @@ class PagesController < ApplicationController
     render html: @page.content.html_safe, layout: false if @page.content_in_html?
   end
 
+  def library
+    @title = I18n.t('page_titles.about.library')
+  end
+
+  def post_order_success
+    @title    = title_for I18n.t('page_titles.about.store'), I18n.t('page_titles.about.post_order_success')
+    @order_id = params[:ordernum]
+  end
+
+  # TODO: make this view localizable
+  def submission_guidelines
+    @title = I18n.t('page_titles.about.submission_guidelines')
+    @title = 'Submission Guidelines'
+  end
+
   private
 
   def set_page
