21de925332237a066b0343cc00bf95ec0215ef0d
Shane Becker <veganstraightedge@gmail.com>
2019-04-09 20:31:50 -0700

    WIP: SSfWD acting like TCE

---
+++ app/controllers/steal_something_from_work_day_controller.rb
@@ -0,0 +1,34 @@
+class StealSomethingFromWorkDayController < ApplicationController
+  layout 'steal_something_from_work_day'
+
+  # TODO
+  # STEAL_SOMETHING_FROM_WORK_DAY_LOCALES = %w[english].freeze
+
+  def show
+    # TODO
+    # @locale = params[:locale]
+
+    # @title = I18n.t('page_titles.about.steal_something_from_work_day')
+    @title = I18n.t('layouts.steal_something_from_work_day.language_direction')
+    @sections = [
+      'Introduction',
+      'FAQ',
+      'Outreach Materials',
+      'Interviews',
+      'Analysis',
+      'Narratives',
+      'Testimonials',
+      'Further Reading',
+      'Selected Coverage',
+      'Internationally'
+    ]
+
+    # TODO
+    # if STEAL_SOMETHING_FROM_WORK_DAY_LOCALES.include?(@locale)
+    #   I18n.locale = @locale
+    # else
+    #   I18n.locale = @locale = I18n.default_locale
+    #   redirect_to [:steal_something_from_work_day]
+    # end
+  end
+end
