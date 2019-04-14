d32ff23cda2fd0c492414c32f9961aba39061256
Shane Becker <veganstraightedge@gmail.com>
2019-04-14 15:55:02 -0700

    Add language switcher to SSfWD

--- app/controllers/steal_something_from_work_day_controller.rb
+++ app/controllers/steal_something_from_work_day_controller.rb
@@ -1,13 +1,23 @@
 class StealSomethingFromWorkDayController < ApplicationController
   layout 'steal_something_from_work_day'
 
-  STEAL_SOMETHING_FROM_WORK_DAY_LOCALES = %i[cz de en es fr].freeze
+  STEAL_SOMETHING_FROM_WORK_DAY_LOCALES = {
+    cz: 'Czech',
+    de: 'Deutsche',
+    en: 'English',
+    es: 'Español',
+    fr: 'Français'
+  }.freeze
 
   def show
-    unless STEAL_SOMETHING_FROM_WORK_DAY_LOCALES.include? I18n.locale
+    unless STEAL_SOMETHING_FROM_WORK_DAY_LOCALES.key? I18n.locale
       I18n.locale = I18n.default_locale
       redirect_to [:steal_something_from_work_day]
     end
+
+    # Remove current locale from language switcher in the view
+    @locales = STEAL_SOMETHING_FROM_WORK_DAY_LOCALES.dup
+    @locales.delete I18n.locale
 
     @sections = [
       'Introduction',
