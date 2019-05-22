92c80cc57cbb14de1b31d54bee8280f92630a7ee
Shane Becker <veganstraightedge@gmail.com>
2019-05-21 23:45:18 -0700

    Remove unused callbacks in podcasts controller

--- app/controllers/podcast_controller.rb
+++ app/controllers/podcast_controller.rb
@@ -1,30 +1,18 @@
 class PodcastController < ApplicationController
-  before_action :set_podcast,  only: [:index, :feed, :show]
-  before_action :set_episodes, only: [:index, :feed]
-
   def index
-    @html_id        = 'page'
-    @body_id        = 'podcast'
-    @podcasts       = Podcast.all.sort_by { |podcast| podcast.latest_episode.published_at }.reverse
-    @editable       = @podcasts.first
-    @title          = title_for :podcasts
+    @html_id  = 'page'
+    @body_id  = 'podcast'
+    @podcasts = Podcast.all.sort_by { |podcast| podcast.latest_episode.published_at }.reverse
+    @editable = @podcasts.first
+    @title    = title_for :podcasts
   end
 
   def show
     @html_id = 'page'
     @body_id = 'podcast'
+    @podcast = Podcast.find_by(slug: params[:slug])
     @title   = title_for @podcast.name
   end
 
   def feed; end
-
-  private
-
-  def set_podcast
-    @podcast  = Podcast.find_by(title: 'The Ex-Worker')
-  end
-
-  def set_episodes
-    @episodes = Episode.live.order(published_at: :desc).to_a
-  end
 end
