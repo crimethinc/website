12bd65a379eb4744cf1f7e6c0199332bf0c4d815
Shane Becker <veganstraightedge@gmail.com>
2019-06-10 11:17:21 -0700

    Add custom NextStepsHelper overcommit hook

---
+++ .git-hooks/post_merge/next_steps_helper.rb
@@ -0,0 +1,44 @@
+module Overcommit
+  module Hook
+    module PostMerge
+      # Checks for changed files in a merge/pull and suggests next steps to run to update dev setup
+      class NextStepsHelper < Base
+        def run
+          changed_files = execute(%w[git diff-tree -r --name-only --no-commit-id ORIG_HEAD HEAD]).stdout.split("\n")
+
+          configs = [
+            # regex            command            explanation
+            [/^Brewfile/,      'brew bundle',     'Brewfile changed'],
+            [%r{^db/migrate/}, 'rake db:migrate', 'new schema migrations']
+          ]
+
+          output_messages = configs.map do |config|
+            regex   = config[0]
+            command = config[1]
+            message = config[2]
+
+            [command, message] if changed_files.map { |file_name| file_name if file_name =~ regex }.compact.any?
+          end
+
+          if output_messages.any?
+            puts
+            puts '#' * 40
+            puts
+            puts '==> Post-merge follow-up commands to run'
+
+            output_messages.each do |command, message|
+              puts "==> Because #{message}, run:"
+              puts "    #{command}"
+              puts
+            end
+
+            puts '#' * 40
+            puts
+          end
+
+          :pass
+        end
+      end
+    end
+  end
+end
