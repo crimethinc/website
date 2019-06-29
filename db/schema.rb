2d68d69bf148db8814cef63b8124760b3222f1d0
Shane Becker <veganstraightedge@gmail.com>
2019-06-28 18:49:23 -0700

    Schema

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,7 @@
 ActiveRecord::Schema.define(version: 2019_06_12_050243) do
 
   # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_stat_statements"
   enable_extension "plpgsql"
 
   create_table "articles", id: :serial, force: :cascade do |t|
