14f3541fd656f0a2a280af4c93684ef9af269cc2
Shane Becker <veganstraightedge@gmail.com>
2019-06-29 01:07:09 -0700

    Reduce Rubocop limits (#1189)
    * Delete commented out rubocop config
    
    * Replace %q{} with HEREDOCs
    
    * Debiggen Metrics/ClassLength from 210 to 128
    
    * Debiggen Metrics/CyclomaticComplexity from 15 to 11
    
    * Debiggen Metrics/LineLengthy from 200 to 186
    
    * Debiggen Metrics/PerceivedComplexity from 15 to 12
    
    * Debiggen RSpec/ExampleLength from 20 to 10
    
    * Debiggen RSpec/MultipleExpectations from 10 to 5

--- db/seeds/pages.rb
+++ db/seeds/pages.rb
@@ -1,13 +1,15 @@
+content = <<~CONTENT
+  - [Who we are and what we do](/about)
+  - [A basic introduction to anarchist values](/tce)
+  - [Frequently asked questions about anarchism](/begin#fa)
+  - [More introductory resources](/begin#fr)
+  - [What you can do](/begin#gs)
+  - [For more coverage of anarchist activity in North America](https://itsgoingdown.org)
+CONTENT
+
 Page.create!(
   title:              'If This Is Your First Time Here&hellip;',
-  content:            %q{
-- [Who we are and what we do](/about)
-- [A basic introduction to anarchist values](/tce)
-- [Frequently asked questions about anarchism](/begin#fa)
-- [More introductory resources](/begin#fr)
-- [What you can do](/begin#gs)
-- [For more coverage of anarchist activity in North America](https://itsgoingdown.org)
-  },
+  content:            content,
   publication_status: 'published',
   published_at:       Time.parse('2017-01-01'),
   slug:               'start',
