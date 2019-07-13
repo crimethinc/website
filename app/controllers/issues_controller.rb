cac812a0fc5d7b4e12f2c454f2a91dc0d3f33015
Shane Becker <veganstraightedge@gmail.com>
2019-07-13 16:58:15 -0700

    Add Issue#show view

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -1,6 +1,8 @@
 class IssuesController < ToolsController
   # See tools_controller.rb for inherited behavior
   def show
-    @issue = Journal.find_by(slug: params[:slug]).issues.find_by(issue: params[:issue_number])
+    journal = Journal.find_by(slug: params[:slug])
+    @issue  = Issue.find_by(issue: params[:issue_number], journal_id: journal.id)
+    @tool   = @issue
   end
 end
--- app/models/issue.rb
+++ app/models/issue.rb
@@ -10,4 +10,17 @@ class Issue < ApplicationRecord
   def path
     "/journals/#{journal.slug}/#{issue}"
   end
+
+  def false_for_missing_methods
+    false
+  end
+
+  alias front_color_image_present? false_for_missing_methods
+  alias front_black_and_white_image_present? false_for_missing_methods
+  alias back_color_image_present? false_for_missing_methods
+  alias back_black_and_white_image_present? false_for_missing_methods
+  alias front_color_download_present? false_for_missing_methods
+  alias front_black_and_white_download_present? false_for_missing_methods
+  alias back_color_download_present? false_for_missing_methods
+  alias back_black_and_white_download_present? false_for_missing_methods
 end
