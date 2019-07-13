16063dcddc23dd63ac7c5379fc17ce7bfa073310
Shane Becker <veganstraightedge@gmail.com>
2019-07-13 14:47:49 -0700

    Add journal_id selection to admin Issue forms

--- app/controllers/admin/issues_controller.rb
+++ app/controllers/admin/issues_controller.rb
@@ -15,6 +15,7 @@ module Admin
 
     def new
       @book = Issue.new
+      @book.journal_id = params[:journal_id]
       @title = admin_title
       render 'admin/books/new'
     end
@@ -54,16 +55,16 @@ module Admin
     end
 
     def journal_params
-      params.require(:journal).permit(:title, :subtitle, :content, :tweet, :summary,
-                                      :description, :buy_url, :buy_info, :slug, :series, :published_at,
-                                      :price_in_cents, :height, :width, :depth, :weight, :pages, :words, :illustrations,
-                                      :photographs, :printing, :ink, :definitions, :recipes, :has_index, :cover_style,
-                                      :binding_style, :table_of_contents, :back_image_present,
-                                      :lite_download_present, :epub_download_present, :mobi_download_present,
-                                      :print_black_and_white_a4_download_present, :print_color_a4_download_present,
-                                      :print_color_download_present, :print_black_and_white_download_present,
-                                      :screen_single_page_view_download_present, :screen_two_page_view_download_present,
-                                      :publication_status)
+      params.require(:issue).permit(:title, :subtitle, :content, :tweet, :summary,
+                                    :description, :buy_url, :buy_info, :slug, :series, :published_at,
+                                    :price_in_cents, :height, :width, :depth, :weight, :pages, :words, :illustrations,
+                                    :photographs, :printing, :ink, :definitions, :recipes, :has_index, :cover_style,
+                                    :binding_style, :table_of_contents, :back_image_present,
+                                    :lite_download_present, :epub_download_present, :mobi_download_present,
+                                    :print_black_and_white_a4_download_present, :print_color_a4_download_present,
+                                    :print_color_download_present, :print_black_and_white_download_present,
+                                    :screen_single_page_view_download_present, :screen_two_page_view_download_present,
+                                    :publication_status, :journal_id)
     end
   end
 end
