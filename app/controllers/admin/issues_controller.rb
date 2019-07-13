fd46b3640c4b292b685ba800dac80a927b1ff828
Shane Becker <veganstraightedge@gmail.com>
2019-07-13 16:29:49 -0700

    Add issue number to admin journal form

--- app/controllers/admin/issues_controller.rb
+++ app/controllers/admin/issues_controller.rb
@@ -64,7 +64,7 @@ module Admin
                                     :print_black_and_white_a4_download_present, :print_color_a4_download_present,
                                     :print_color_download_present, :print_black_and_white_download_present,
                                     :screen_single_page_view_download_present, :screen_two_page_view_download_present,
-                                    :publication_status, :journal_id)
+                                    :publication_status, :journal_id, :issue)
     end
   end
 end
