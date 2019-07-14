af6f63c9e42d62495b2091a2e386f8012ad4ec32
Shane Becker <veganstraightedge@gmail.com>
2019-07-13 17:05:20 -0700

    Add editable to Issues#show

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -1,8 +1,9 @@
 class IssuesController < ToolsController
   # See tools_controller.rb for inherited behavior
   def show
-    journal = Journal.find_by(slug: params[:slug])
-    @issue  = Issue.find_by(issue: params[:issue_number], journal_id: journal.id)
-    @tool   = @issue
+    journal   = Journal.find_by(slug: params[:slug])
+    @issue    = Issue.find_by(issue: params[:issue_number], journal_id: journal.id)
+    @tool     = @issue
+    @editable = @issue
   end
 end
