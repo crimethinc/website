40b5707597c4db1d3930a620715bda37718d9b0b
wbholland <39104153+wbholland@users.noreply.github.com>
2019-09-03 23:26:36 -0400

    use querySelector and setter methods
    - changed querySelectorAll() to querySelector() since there should be only one #floating-alert
    - now using setter methods setAttribute(), classList.add(), and insertAdjacentHTML() instead of manipulating object fields directly

--- app/assets/javascripts/articles.js
+++ app/assets/javascripts/articles.js
@@ -1,10 +1,10 @@
 App.articleQueue.onIncoming = function(data) {
-  var alertDiv = document.querySelectorAll('#floating-alert');
+  var alertDiv = document.querySelector('#floating-alert');
 
-  if (alertDiv.length === 0) {
+  if (alertDiv === null) {
     alertDiv = document.createElement('div');
-    alertDiv.id = 'floating-alert';
-    alertDiv.className = 'alert alert-floating alert-notice';
+    alertDiv.setAttribute('id', 'floating-alert');
+    alertDiv.classList.add('alert', 'alert-floating', 'alert-notice');
     
     document.body.prepend(alertDiv);
   }
@@ -12,10 +12,10 @@ App.articleQueue.onIncoming = function(data) {
   var postPlural = data.length > 1 ? 'posts' : 'post';
   
   var closeLink = document.createElement('a');
-  closeLink.className = 'close';
-  closeLink.innerHTML = '&#10006;';
+  closeLink.classList.add('close');
+  closeLink.insertAdjacentHTML('afterbegin', '&#10006;');
   closeLink.addEventListener('click', function() { alertDiv.remove() });
   
-  alertDiv.innerHTML = 'Psst! Refresh for ' + data.length + ' new ' + postPlural;
+  alertDiv.insertAdjacentHTML('afterbegin', 'Psst! Refresh for ' + data.length + ' new ' + postPlural);
   alertDiv.appendChild(closeLink);
 };
