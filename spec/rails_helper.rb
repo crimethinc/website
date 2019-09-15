1f6aeebb6881e36afe263addc8a757489dfcd317
Shane Becker <veganstraightedge@gmail.com>
2019-09-14 17:32:32 -0700

    Use headless Firefox for specs instead of chrome (#1279)
    Chrome started having errors in CI, so we switched to FF.
    
    I am sure at some point in the future we will have an issue with
    FF and then move back to chrome

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -18,13 +18,7 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
 end
 
-Capybara.register_driver :chrome do |app|
-  options = Selenium::WebDriver::Chrome::Options.new(args: %w[no-sandbox headless disable-gpu])
-
-  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
-end
-
-Capybara.javascript_driver = :chrome
+Capybara.javascript_driver = :selenium_headless
 
 # from https://github.com/rspec/rspec-rails/issues/1897
 Capybara.server = :puma, { Silent: true }
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,7 @@ SimpleCov.start 'rails'
 
 RSpec.configure do |config|
   config.before(:each, type: :system) do
-    driven_by :chrome
+    driven_by :selenium_headless, using: :firefox
   end
 
   config.expect_with :rspec do |expectations|
