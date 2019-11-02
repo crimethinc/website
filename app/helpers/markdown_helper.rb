a233900867ecafa8ebedda37071c17db53b8222f
Shane Becker <veganstraightedge@gmail.com>
2019-11-02 14:39:25 -0700

    Add remove_wrapper_paragraph_tag arg to MarkdownHelper#render_markdown

--- app/helpers/markdown_helper.rb
+++ app/helpers/markdown_helper.rb
@@ -5,14 +5,30 @@ module MarkdownHelper
     render_markdown content
   end
 
-  def render_markdown text
-    return if text.blank?
+  def render_markdown markdown, remove_wrapper_paragraph_tags: false
+    return if markdown.blank?
 
-    Kramdown::Document.new(
-      MarkdownMedia.parse(text, include_media: media_mode?),
+    html = Kramdown::Document.new(
+      MarkdownMedia.parse(markdown, include_media: media_mode?),
       input:                     :kramdown,
       remove_block_html_tags:    false,
       transliterated_header_ids: true
     ).to_html.html_safe
+
+    remove_wrapper_paragraph_tags ? html : remove_wrapper_paragraph_tag(html).html_safe
+
+    html
+  end
+
+  def remove_wrapper_paragraph_tag html
+    html_doc = Nokogiri::HTML(html.chomp)
+
+    outer_paragraph = html_doc.css('html body p:first')
+
+    if outer_paragraph.present?
+      outer_paragraph.first.inner_html
+    else
+      html
+    end
   end
 end
