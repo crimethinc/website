04e233255e8b9bad0ce091a67c49caafaf179c90
Shane Becker <veganstraightedge@gmail.com>
2019-11-03 17:51:57 -0800

    Theme up: journals / issues

--- app/controllers/journals_controller.rb
+++ app/controllers/journals_controller.rb
@@ -2,9 +2,11 @@ class JournalsController < ToolsController
   # See tools_controller.rb for inherited behavior
   def index
     @journals = Journal.includes(:issues)
+    render "#{Theme.name}/journals/index"
   end
 
   def show
     @journal = Journal.find_by(slug: params[:id])
+    render "#{Theme.name}/journals/show"
   end
 end
