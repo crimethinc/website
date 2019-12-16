f59a1339d43d0aa867badf560ede6a81613a1e46
Shane Becker <veganstraightedge@gmail.com>
2019-12-15 20:31:37 -0800

    Theme languages pages

--- app/controllers/languages_controller.rb
+++ app/controllers/languages_controller.rb
@@ -3,11 +3,15 @@ class LanguagesController < ApplicationController
     @html_id = 'page'
     @body_id = 'languages'
     @locales = Locale.all
+
+    render "#{Theme.name}/languages/index"
   end
 
   def show
     @html_id = 'page'
     @body_id = 'languages'
     @locale  = Locale.find_by slug: params[:locale]
+
+    render "#{Theme.name}/languages/show"
   end
 end
