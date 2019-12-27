628715aa002d7d160199dd3ab052bb367788a84c
Shane Becker <veganstraightedge@gmail.com>
2019-12-27 16:53:05 -0600

    DRY sections TOC lists

--- app/controllers/to_change_everything_controller.rb
+++ app/controllers/to_change_everything_controller.rb
@@ -3,8 +3,14 @@ class ToChangeEverythingController < ApplicationController
 
   TO_CHANGE_ANYTHING_YAMLS = %w[日本語 portugues quebecois espanol-america-latina lietuvos 한국어 english espanol فارسی].freeze
 
+  SECTIONS_FIRST_GROUP = %w[introduction self answering power relationships reconciling liberation revolt control].freeze
+  SECTIONS_LAST_GROUP  = %w[hierarchy borders representation leaders government profit property lastcrime anarchy outro takeflight next].freeze
+
   def show
     @locale = params[:lang]
+
+    @sections_first_group = SECTIONS_FIRST_GROUP
+    @sections_last_group  = SECTIONS_LAST_GROUP
 
     if TO_CHANGE_ANYTHING_YAMLS.include?(@locale)
       I18n.locale = @locale
