a3c8f4d3fbd81da322249d664fbffe71606549fd
astronaut-wannabe <contact@astronaut-wannabe.com>
2019-12-27 20:14:11 -0800

    [API] Allow certain endpoints to return JSON
    In [Pull Request #1418][0] I blocked ALL json requests
    
    We actually have a few endpoints we want to let through
    
    [0]:https://github.com/crimethinc/website/pull/1418

--- spec/requests/json_requests_spec.rb
+++ spec/requests/json_requests_spec.rb
@@ -17,4 +17,51 @@ describe 'JsonRequests', type: :request do
     error_message = JSON.parse(body)['errors'].first.dig('detail')
     expect(error_message).to eq expected_error
   end
+
+  context 'when there is no HTTP_ACCEPT header' do
+    let(:json_headers) { { HTTP_ACCEPT: nil } }
+
+    it 'continues through to the regular app' do
+      get root_path, headers: json_headers
+
+      expect(response.content_type).to eq 'text/html'
+      expect(status).to eq 301
+    end
+  end
+
+  context 'when hitting the article polling endpoint' do
+    let(:published_at) { 5.minutes.ago }
+    let(:collection) { create(:article, title: 'test', publication_status: 'published', published_at: published_at) }
+    let!(:article) do
+      create(:article, title: 'collection', collection_id: collection.id, publication_status: 'published', published_at: published_at)
+    end
+
+    it 'still returns collections' do
+      get "/articles/#{collection.id}/collection_posts", params: { published_at: published_at.to_i }, headers: json_headers
+      follow_redirect!
+
+      article_title = JSON.parse(response.body)['posts'].first['title']
+      expect(article_title).to eq article.title
+    end
+  end
+
+  context 'when hitting the json manifet path' do
+    it 'returns the manifest' do
+      get '/manifest.json', headers: json_headers
+      follow_redirect!
+      expect(status).to eq 200
+    end
+  end
+
+  context 'when hitting the JSON RSS feed' do
+    before { create(:article, content: 'foo') }
+
+    it 'returns the feed' do
+      get '/feed.json', headers: json_headers
+      follow_redirect!
+
+      expect(status).to eq 200
+      expect(response.body).to include 'foo'
+    end
+  end
 end
