6cbea82eaaf8a3ba40b7d741ed939d74effb4134
Shane Becker <veganstraightedge@gmail.com>
2019-12-29 14:46:51 -0600

    Add #display_name spec

--- spec/models/locale_spec.rb
+++ spec/models/locale_spec.rb
@@ -10,4 +10,14 @@ RSpec.describe Locale, type: :model do
       expect(spanish_locale).not_to be_english
     end
   end
+
+  describe '#display_name' do
+    let(:english_locale) { build :locale, :en }
+    let(:spanish_locale) { build :locale, :es }
+
+    it 'formats for display' do
+      expect(english_locale.display_name).to eq 'EN : English / English'
+      expect(spanish_locale.display_name).to eq 'ES : Spanish / Español'
+    end
+  end
 end
