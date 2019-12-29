805acb12d322e8adce39e978916a0164584ca545
Shane Becker <veganstraightedge@gmail.com>
2019-12-29 13:28:28 -0600

    Add articles_count to Locales table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_15_191508) do
+ActiveRecord::Schema.define(version: 2019_12_29_192638) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_stat_statements"
@@ -226,6 +226,7 @@ ActiveRecord::Schema.define(version: 2019_12_15_191508) do
     t.datetime "updated_at", null: false
     t.integer "language_direction", default: 0
     t.string "slug"
+    t.integer "articles_count", default: 0
   end
 
   create_table "logos", force: :cascade do |t|
