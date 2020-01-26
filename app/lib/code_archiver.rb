77a55c09e5ff4a79777f21e5bf13e2aaea5f5cf1
astronaut-wannabe <astronaut-wannabe@users.noreply.github.com>
2020-01-25 20:25:20 -0600

    [CODE VAULT] change archive (#1482)
    Now that we've got some of this archiver stuff in place, we decided to
    change up the directory structure to be like this:
    
    ```
    .
    ├── articles
    │   ├── 1996
    │   │   └── 01
    │   │       └── 01
    │   │           ├── how-ethical-is-the-work-ethic.html
    │   │           ├── how-ethical-is-the-work-ethic.md
    │   │           ├── how-i-spent-my-permanent-vacation.html
    │   │           ├── how-i-spent-my-permanent-vacation.md
    │   │           ├── introduction-to-the-situationists.html
    │   │           ├── introduction-to-the-situationists.md
    │   │           └── selling-ourselves-out.html
    │   │           └── selling-ourselves-out.md
    
    └── pages
        ├── about.html
        ├── about.md
        └── faq.html
        └── faq.md
    ```
    
    This sets us up to be able to put the images (etc) in the same
    articles "day" folder and use them easily by both md and html files.

--- app/lib/code_archiver.rb
+++ app/lib/code_archiver.rb
@@ -2,36 +2,34 @@ class CodeArchiver
   class << self
     def put_it_on_ice # rubocop:disable Metrics/MethodLength
       # make all the directories
-      system 'mkdir', '-p', html_prefix
-      system 'mkdir', '-p', md_prefix
-      system 'mkdir', '-p', html_prefix(type: 'pages')
-      system 'mkdir', '-p', md_prefix(type: 'pages')
+      system 'mkdir', '-p', articles_prefix
+      system 'mkdir', '-p', pages_prefix
 
       puts '**********************************************************************'
       puts 'Making html article files'
       Article.published.live.in_batches.each_record do |article|
-        make_article_dir article, type: :html
+        make_article_dir article
         make_html_article article
       end
 
       puts '**********************************************************************'
       puts 'Making markdown article files'
       Article.published.live.in_batches.each_record do |article|
-        make_article_dir article, type: :md
+        make_article_dir article
         make_md_article article
       end
 
       puts '**********************************************************************'
       puts 'Making html page files'
       Page.where(slug: %i[about faq]).each do |page|
-        make_page_dir page, type: :html
+        make_page_dir page
         make_html_page page
       end
 
       puts '**********************************************************************'
       puts 'Making markdown page files'
       Page.where(slug: %i[about faq]).each do |page|
-        make_page_dir page, type: :md
+        make_page_dir page
         make_md_page page
       end
 
@@ -42,18 +40,16 @@ class CodeArchiver
 
     private
 
-    def make_article_dir article, type: :html
-      system 'mkdir', '-p', "#{html_prefix}/#{article_dir(article)}" if type == :html
-      system 'mkdir', '-p', "#{md_prefix}/#{article_dir(article)}" if type == :md
+    def make_article_dir article
+      system 'mkdir', '-p', "#{articles_prefix}/#{article_dir(article)}"
     end
 
-    def make_page_dir page, type: :html
-      system 'mkdir', '-p', "#{html_prefix(type: 'pages')}/#{page.slug}}" if type == :html
-      system 'mkdir', '-p', "#{md_prefix(type: 'pages')}/#{page.slug}" if type == :md
+    def make_page_dir page
+      system 'mkdir', '-p', "#{pages_prefix}/#{page.slug}}"
     end
 
     def make_html_article article
-      File.open("#{html_prefix}/#{article_dir(article)}/#{article.slug}.html", 'w') do |file|
+      File.open("#{articles_prefix}/#{article_dir(article)}/#{article.slug}.html", 'w') do |file|
         file.puts "<h1>#{article.title}</h1>"
         file.puts "<h2>#{article.subtitle}</h2>" unless article.subtitle.blank?
         file.puts to_html article
@@ -61,7 +57,7 @@ class CodeArchiver
     end
 
     def make_md_article article
-      File.open("#{md_prefix}/#{article_dir(article)}/#{article.slug}.md", 'w') do |file|
+      File.open("#{articles_prefix}/#{article_dir(article)}/#{article.slug}.md", 'w') do |file|
         file.puts "# #{article.title}"
         file.puts "## #{article.subtitle}" unless article.subtitle.blank?
         file.puts to_markdown article
@@ -69,7 +65,7 @@ class CodeArchiver
     end
 
     def make_html_page page
-      File.open("#{html_prefix(type: 'pages')}/#{page.slug}.html", 'w') do |file|
+      File.open("#{pages_prefix}/#{page.slug}.html", 'w') do |file|
         file.puts "<h1>#{page.title}</h1>"
         file.puts "<h2>#{page.subtitle}</h2>" unless page.subtitle.blank?
         file.puts to_html page
@@ -77,7 +73,7 @@ class CodeArchiver
     end
 
     def make_md_page page
-      File.open("#{md_prefix(type: 'pages')}/#{page.slug}.md", 'w') do |file|
+      File.open("#{pages_prefix}/#{page.slug}.md", 'w') do |file|
         file.puts "# #{page.title}"
         file.puts "## #{page.subtitle}" unless page.subtitle.blank?
         file.puts to_markdown page
@@ -106,15 +102,6 @@ class CodeArchiver
       ).to_kramdown
     end
 
-    def to_pdf article
-      Kramdown::Document.new(
-        MarkdownMedia.parse(article.content, include_media: true),
-        input:                     :kramdown,
-        remove_block_html_tags:    false,
-        transliterated_header_ids: true
-      ).to_latex
-    end
-
     def git_steps
       <<~HEREDOC
         cd website-content/ && \\\n
@@ -129,12 +116,12 @@ class CodeArchiver
       HEREDOC
     end
 
-    def html_prefix type: 'articles'
-      "website-content/html/#{type}"
+    def articles_prefix
+      'website-content/articles'
     end
 
-    def md_prefix type: 'articles'
-      "website-content/markdown/#{type}"
+    def pages_prefix
+      'website-content/pages'
     end
   end
 end
