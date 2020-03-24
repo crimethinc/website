3b587138578518d308a6201c489551d38bddcc7a
Shane Becker <veganstraightedge@gmail.com>
2020-03-24 00:30:50 -0700

    NOT DRY: Support current locales as subdomains on cwc.im

--- app/middlewares/rack/domain_redirect.rb
+++ app/middlewares/rack/domain_redirect.rb
@@ -2,13 +2,34 @@ module Rack
   class DomainRedirect
     # subdomain (optional), path prefix (optional), URL regex to match (required)
     REDIRECT_CONFIGS = [
-      ['',    '/steal-something-from-work-day', /stealfromwork.crimethinc.com|stealfromworkday.com|stealsomethingfromworkday.com$/],
-      ['',    '/tce',                           /tochangeeverything.com/],
-      ['id.', '',                               /id.cwc.im$/], # TEMP: work out better general purpose locale subdomain + short domain redirect
-      ['',    '',                               /cwc|crimethinc.herokuapp.com$/],
-      ['es.', '',                               /crimethinc.es/],
-      ['de.', '',                               /crimethinc.de/],
-      ['cs.', '',                               /cz.crimethinc.com/]
+      ['', '/steal-something-from-work-day', /stealfromwork.crimethinc.com|stealfromworkday.com|stealsomethingfromworkday.com$/],
+      ['', '/tce',                           /tochangeeverything.com/],
+
+      ['',    '', /crimethinc.herokuapp.com$/],
+      ['es.', '', /crimethinc.es/],
+      ['de.', '', /crimethinc.de/],
+      ['cs.', '', /cz.crimethinc.com/], # Fix our orignal mistaken assumption
+
+      ['ar.', '', /ar.cwc.im$/], # TEMP: work out better general purpose locale subdomain + short domain redirect
+      ['cs.', '', /cs.cwc.im$/], # TEMP: work out better general purpose locale subdomain + short domain redirect
+      ['cz.', '', /cz.cwc.im$/], # TEMP: work out better general purpose locale subdomain + short domain redirect
+      ['da.', '', /da.cwc.im$/], # TEMP: work out better general purpose locale subdomain + short domain redirect
+      ['de.', '', /de.cwc.im$/], # TEMP: work out better general purpose locale subdomain + short domain redirect
+      ['en.', '', /en.cwc.im$/], # TEMP: work out better general purpose locale subdomain + short domain redirect
+      ['es.', '', /es.cwc.im$/], # TEMP: work out better general purpose locale subdomain + short domain redirect
+      ['fi.', '', /fi.cwc.im$/], # TEMP: work out better general purpose locale subdomain + short domain redirect
+      ['fr.', '', /fr.cwc.im$/], # TEMP: work out better general purpose locale subdomain + short domain redirect
+      ['gr.', '', /gr.cwc.im$/], # TEMP: work out better general purpose locale subdomain + short domain redirect
+      ['he.', '', /he.cwc.im$/], # TEMP: work out better general purpose locale subdomain + short domain redirect
+      ['id.', '', /id.cwc.im$/], # TEMP: work out better general purpose locale subdomain + short domain redirect
+      ['in.', '', /in.cwc.im$/], # TEMP: work out better general purpose locale subdomain + short domain redirect
+      ['it.', '', /it.cwc.im$/], # TEMP: work out better general purpose locale subdomain + short domain redirect
+      ['pl.', '', /pl.cwc.im$/], # TEMP: work out better general purpose locale subdomain + short domain redirect
+      ['pt.', '', /pt.cwc.im$/], # TEMP: work out better general purpose locale subdomain + short domain redirect
+      ['ru.', '', /ru.cwc.im$/], # TEMP: work out better general purpose locale subdomain + short domain redirect
+      ['sv.', '', /sv.cwc.im$/], # TEMP: work out better general purpose locale subdomain + short domain redirect
+      ['tr.', '', /tr.cwc.im$/], # TEMP: work out better general purpose locale subdomain + short domain redirect
+      ['',    '', /cwc.im/]
     ].freeze
 
     PROTOCOL = 'https://'.freeze
@@ -27,8 +48,8 @@ module Rack
         url_regex   = config[2]
         path        = path_prefix + request.path
 
-        if url_regex.match?(request.host.downcase)
-          location = redirect_location(subdomain: subdomain, path: path)
+        if url_regex.match? request.host.downcase
+          location = redirect_location subdomain: subdomain, path: path
           return redirect location
         end
       end
