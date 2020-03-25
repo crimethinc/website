1bd1712dd790022bd4aa643310b7ead94fefd441
Shane Becker <veganstraightedge@gmail.com>
2020-03-24 18:48:46 -0700

    ERB lint

--- app/assets/javascripts/articles.js
+++ app/assets/javascripts/articles.js
@@ -5,17 +5,17 @@ App.articleQueue.onIncoming = function(data) {
     alertDiv = document.createElement('div');
     alertDiv.setAttribute('id', 'floating-alert');
     alertDiv.classList.add('alert', 'alert-floating', 'alert-notice');
-    
+
     document.body.prepend(alertDiv);
   }
 
   var postPlural = data.length > 1 ? 'posts' : 'post';
-  
+
   var closeLink = document.createElement('a');
   closeLink.classList.add('close');
   closeLink.insertAdjacentHTML('afterbegin', '&#10006;');
   closeLink.addEventListener('click', function() { alertDiv.remove() });
-  
+
   alertDiv.insertAdjacentHTML('afterbegin', `Psst! Refresh for ${data.length} new ${postPlural}`);
   alertDiv.appendChild(closeLink);
 };
