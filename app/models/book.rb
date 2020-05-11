fd8a4a569a30f60f145d524dbede0c186d7a0a4c
astronaut-wannabe <contact@astronaut-wannabe.com>
2020-05-10 21:07:23 -0700

    Add a `position` field to books
    backfill script:
    
    This script isn't that important as we can always manually fiddle with
    the positions in production
    
    ```ruby
    BOOK_SLUGS_IN_ORDER = %w[no-wall-they-can-build
                             from-democracy-to-freedom
                             contradictionary
                             work
                             expect-resistance
                             recipes-for-disaster
                             days-of-war-nights-of-love
                             off-the-map
                             no-habra-muro-que-nos-pare
                             da-democracia-a-liberdade
                             trabalho-edicao-resumida-de-emergencia
                             espere-resistencia
                             receitas-para-o-desastre
                             dias-de-guerra-noites-de-amor].freeze
    
    BOOK_SLUGS_IN_ORDER.each_with_index do |slug, index|
      Book.find_by(slug: slug)&.update!(position: index)
    end
    ```
    
    resolves https://github.com/crimethinc/website/issues/1446

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,6 +2,8 @@ class Book < ApplicationRecord
   include MultiPageTool
   include Featureable
 
+  scope :for_index, -> { where.not(hide_from_index: true).reorder(position: :asc).order(title: :asc) }
+
   def ask_for_donation?
     downloads_available?
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_05_003649) do
+ActiveRecord::Schema.define(version: 2020_05_11_024239) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_stat_statements"
@@ -98,6 +98,8 @@ ActiveRecord::Schema.define(version: 2020_04_05_003649) do
     t.integer "canonical_id"
     t.boolean "featured_status", default: false
     t.datetime "featured_at"
+    t.integer "position"
+    t.boolean "hide_from_index", default: false
     t.index ["canonical_id"], name: "index_books_on_canonical_id"
   end
 
