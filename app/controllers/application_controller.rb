e62cc8794dfad6411f23191cff902ab5a74b87fa
Shane Becker <veganstraightedge@gmail.com>
2020-05-24 15:29:01 -0700

    Remove current_user as a helper method, no longer used in views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -43,7 +43,6 @@ class ApplicationController < ActionController::Base
   def current_user
     Current.user ||= User.find(session[:user_id]) if session[:user_id]
   end
-  helper_method :current_user
 
   def authorize
     redirect_to [:signin], alert: 'You need to sign in to view that page.' unless signed_in?
