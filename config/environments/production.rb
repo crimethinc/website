6ba3327f1c7ac6362a0828e6e86c1bf57be86464
Shane Becker <veganstraightedge@gmail.com>
2020-07-15 00:02:16 -0700

    Only use Sidekiq for background jobs in Production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Background jobs
+  config.active_job.queue_adapter = :sidekiq
 end
