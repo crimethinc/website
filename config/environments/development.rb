128b3ce046575e76365fc907543933f0cb48a9a0
Shane Becker <veganstraightedge@gmail.com>
2020-08-09 18:40:43 -0700

    Add host to environment configs

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,4 +59,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # For using #url_for et al in non-views/helpers
+  Rails.application.routes.default_url_options[:host] = 'localhost:3000'
 end
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -40,4 +40,7 @@ Rails.application.configure do
 
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
+
+  # For using #url_for et al in non-views/helpers
+  Rails.application.routes.default_url_options[:host] = 'localhost:3000'
 end
