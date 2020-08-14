dc8ce73f4c40d681438af7d356bbc16aad8a485a
Shane Becker <veganstraightedge@gmail.com>
2020-08-14 02:00:47 -0700

    Use ActiveStorage uploads in SinglePageTool concern (Poster/Sticker)

--- app/models/concerns/single_page_tool.rb
+++ app/models/concerns/single_page_tool.rb
@@ -5,90 +5,79 @@ module SinglePageTool
   include Translatable
 
   def image_description
-    I18n.t('tools.poster.image_description', title: title)
+    I18n.t 'tools.poster.image_description', title: title
   end
   alias front_image_description image_description
 
   def back_image_description
-    I18n.t('tools.poster.image_description', title: title)
-  end
-
-  def front_color_image
-    image side: :front, color: :color
-  end
-
-  def front_black_and_white_image
-    image side: :front, color: :black_and_white
-  end
-
-  def back_color_image
-    image side: :back, color: :color
-  end
-
-  def back_black_and_white_image
-    image side: :back, color: :black_and_white
-  end
-
-  def image side: :front, color: :color
-    if send("#{side}_#{color}_image_present?")
-      filename = [slug, '_', side, '_', color, '.', send("#{side}_image_format")].join
-      return [asset_base_url_prefix, filename].join('/')
-    end
-
-    file_name = []
-    file_name << slug
-    file_name << '_'
-
-    file_name <<
-      if front_color_image_present?
-        'front_color'
-      elsif front_black_and_white_image_present?
-        'front_black_and_white'
-      elsif back_color_image_present?
-        'back_color'
-      elsif back_black_and_white_image_present?
-        'back_black_and_white'
-      end
-
-    file_name << '.'
-    file_name << send("#{side}_image_format")
-
-    [asset_base_url_prefix, file_name.join].join('/')
+    I18n.t 'tools.poster.image_description', title: title
   end
 
   def front_image
-    front_image_present? ? preferred_front_image : fallback_image_url(side: :front)
+    # TODO
+    image_url side: :front, color: preferred_front_image_color
+  end
+  alias meta_image front_image
+  alias image front_image
+
+  def download_url side: nil, color: nil
+    url_of side: side, color: color, kind: :download
   end
 
-  def back_image
-    back_image_present? ? preferred_back_image : fallback_image_url(side: :back)
+  private
+
+  # URL builders
+  def image_url side: :front, color: :color
+    url_of side: side, color: color, kind: :image
   end
 
-  def fallback_image_url side:
-    [asset_base_url_prefix, "#{slug}_#{side}.#{send("#{side}_image_format")}"].join('/')
+  def url_of side:, color:, kind:
+    Rails.application.routes.url_helpers.rails_blob_url send("image_#{side}_#{color}_#{kind}")
   end
 
-  def preferred_front_image
-    front_color_image_present ? front_color_image : front_black_and_white_image
+  def preferred_front_image_color
+    return :color           if image_front_color_image.attached?           || image_front_color_download.attached?
+    return :black_and_white if image_front_black_and_white_image.attached? || image_front_black_and_white_download.attached?
+
+    :color
   end
 
-  def preferred_back_image
-    back_color_image_present ? back_color_image : back_black_and_white_image
+  def preferred_front_image_kind
+    return :image    if front_image_present?
+    return :download if front_download_present?
+
+    :download
+  end
+
+  def front_color_image
+    image_url side: :front, color: :color
+  end
+
+  def front_black_and_white_image
+    image_url side: :front, color: :black_and_white
+  end
+
+  def back_color_image
+    image_url side: :back, color: :color
+  end
+
+  def back_black_and_white_image
+    image_url side: :back, color: :black_and_white
   end
 
   def front_image_present?
-    front_color_image_present? || front_black_and_white_image_present?
+    image_front_color_image.attached? || image_front_black_and_white_image.attached?
   end
 
   def back_image_present?
-    back_color_image_present? || back_black_and_white_image_present?
+    image_back_color_image.attached? || image_back_black_and_white_image.attached?
   end
 
-  def download_url side: nil, color: nil
-    Rails.application.routes.url_helpers.rails_blob_url send("image_#{side}_#{color}_download")
+  def front_download_present?
+    image_front_color_download.attached? || image_front_black_and_white_download.attached?
   end
 
-  def meta_image
-    front_image
+  def back_download_present?
+    image_back_color_download.attached? || image_back_black_and_white_download.attached?
   end
 end
