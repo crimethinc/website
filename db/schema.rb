61c85a6dd252666c0f4af21ff8eee0755f436d47
Shane Becker <veganstraightedge@gmail.com>
2020-08-20 20:46:44 -0700

    Fix seeds script typo

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,7 @@
 ActiveRecord::Schema.define(version: 2020_07_15_082914) do
 
   # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_stat_statements"
   enable_extension "plpgsql"
 
   create_table "active_storage_attachments", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,13 @@
 puts '==> Downloading DB dump from S3…'
-sh 'rake db:import:download'
+# sh 'rake db:import:download'
 puts
 
 puts '==> Importing pg dump into local development DB…'
-sh 'rake db:import:populate'
+# sh 'rake db:import:populate'
 puts
 
 puts '==> Importing uploads into local development DB…'
-system! 'rake seed:uploads:import'
+sh 'rake seed:uploads:import'
 puts
 
 puts '==> All done!'
