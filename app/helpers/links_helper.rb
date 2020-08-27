5efb52d8b74db90b14d42eff566a664c697008aa
Shane Becker <veganstraightedge@gmail.com>
2020-08-27 00:25:28 -0700

    Add Tumblr and Mastodon to social links in footer (#1749)
    * Update FB URL in footer
    
    * Add Tumblr link to footer
    
    * Lint social SVG files
    
    * Add Mastodon social icon link to footer

--- app/helpers/links_helper.rb
+++ app/helpers/links_helper.rb
@@ -2,9 +2,11 @@ module LinksHelper
   def social_links
     {
       'CrimethInc. on Twitter'           => 'https://twitter.com/crimethinc',
-      'CrimethincDotCom on Facebook'     => 'https://facebook.com/CrimethincDotCom',
+      'CrimethincDotCom on Facebook'     => 'https://www.facebook.com/To-Change-Everything-103734818122357',
       'CrimethincDotCom on Instagram'    => 'https://instagram.com/CrimethincDotCom',
-      'CrimethInc.com on Github'         => 'https://github.com/crimethinc',
+      'CrimethInc on Github'             => 'https://github.com/crimethinc',
+      'CrimethInc on Tumblr'             => 'https://crimethinc.tumblr.com',
+      'CrimethInc on Mastodon'           => 'https://mastodon.online/@crimethinc',
       'CrimethInc.com Articles RSS feed' => 'https://crimethinc.com/feed'
     }
   end
@@ -13,8 +15,9 @@ module LinksHelper
     domain = URI.parse(url).host.downcase.split('.')[-2]
     domain = "link-domain-#{domain}"
 
-    name   = name.downcase.to_slug
-    name   = "link-name-#{name}"
+    name = name.downcase.to_slug
+    name = "link-name-#{name}"
+
     [name, domain].join(' ')
   end
 end
