ca5ed078c803cd2dc684040322b4862431cf8cd4
Shane Becker <veganstraightedge@gmail.com>
2020-08-27 00:20:00 -0700

    Add Mastodon social icon link to footer

--- app/helpers/links_helper.rb
+++ app/helpers/links_helper.rb
@@ -4,8 +4,9 @@ module LinksHelper
       'CrimethInc. on Twitter'           => 'https://twitter.com/crimethinc',
       'CrimethincDotCom on Facebook'     => 'https://www.facebook.com/To-Change-Everything-103734818122357',
       'CrimethincDotCom on Instagram'    => 'https://instagram.com/CrimethincDotCom',
-      'CrimethInc.com on Github'         => 'https://github.com/crimethinc',
-      'CrimethInc.com on Tumblr'         => 'https://crimethinc.tumblr.com',
+      'CrimethInc on Github'             => 'https://github.com/crimethinc',
+      'CrimethInc on Tumblr'             => 'https://crimethinc.tumblr.com',
+      'CrimethInc on Mastodon'           => 'https://mastodon.online/@crimethinc',
       'CrimethInc.com Articles RSS feed' => 'https://crimethinc.com/feed'
     }
   end
