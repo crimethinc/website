d03cca0f9f6fcf81505b19c1c1dba3cfff6e17bf
Shane Becker <veganstraightedge@gmail.com>
2020-10-08 13:34:15 -0700

    Add Honeycomb for observability (#1802)
    * Add Honeycomb gem
    
    * Add Honeycomb initializer
    
    * Set environment specific dataset name
    
    * Add Current.theme to each request
    
    * Filter out username
    
    * Fasterer
    
    * Extract class Event, as Honeycomb vendor wrapper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,6 +12,7 @@ class ApplicationController < ActionController::Base
   before_action :strip_file_extension
   before_action :authorize, if: :staging?
   before_action :set_page_share
+  before_action :add_global_event_fields
 
   helper :meta
 
@@ -120,6 +121,10 @@ class ApplicationController < ActionController::Base
   end
 
   private
+
+  def add_global_event_fields
+    Event.add_field :current_theme, Current.theme
+  end
 
   def page_share_url
     # TODO: implement this algorithm
