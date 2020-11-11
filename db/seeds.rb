c1a4c72305c67660e5febf74685215efcaad04fb
Shane Becker <veganstraightedge@gmail.com>
2020-11-10 22:49:38 -0800

    Re-enable importing and populate db in db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
 puts '==> Downloading DB dump from S3…'
-# sh 'rake db:import:download'
+sh 'rake db:import:download'
 puts
 
 puts '==> Importing pg dump into local development DB…'
-# sh 'rake db:import:populate'
+sh 'rake db:import:populate'
 puts
 
 puts '==> Importing uploads into local development DB…'
