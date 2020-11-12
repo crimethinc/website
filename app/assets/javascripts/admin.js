9f4ea1b8a55554195ffa6ff6749e850c65ae5592
astronaut-wannabe <contact@astronaut-wannabe.com>
2020-11-12 10:51:06 -0800

    Remove JQuery from the tweet length counter
    closes: https://github.com/crimethinc/website/issues/1864

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -5,24 +5,29 @@
 //= require jquery_ujs
 //= require popper
 //= require bootstrap
+//= require dom_ready
 
 // character counter on textareas
-$(function() {
-  return $("textarea[data-max-length]").keyup(function(e) {
-    e.preventDefault();
+function updateCounter(e) {
+  e.preventDefault();
 
-    var text_length = e.currentTarget.value.length;
-    var text_max, id_to_update;
+  var text_length = e.currentTarget.value.length;
+  var text_max, id_to_update;
 
-    id_to_update = $(this).data("feedback-box");
-    text_max = $(this).data("max-length");
+  id_to_update = this.dataset.feedbackBox;
+  text_max = this.dataset.maxLength;
 
-    var text_remaining = text_max - text_length;
-    return $("#"+id_to_update).html(text_remaining);
-  });
-});
+  var text_remaining = text_max - text_length;
+  var counter = document.getElementById(id_to_update)
+  counter.textContent = text_remaining;
+  return counter;
+}
 
-$(function() {
+function polyfillDateAndTime() {
+  // Check if the browser supports the native date/time input types
+  // This should be all browsers besides IE11 and the desktop version
+  // of safari If the browser doesn't support it, we polyfill the
+  // widgets (this uses JQuery)
   var date_and_time_supported = (Modernizr.inputtypes.time && Modernizr.inputtypes.date);
   // not-supported, so polyfill
   if (!date_and_time_supported) {
@@ -39,4 +44,10 @@ $(function() {
       scrollbar: true
     });
   }
+}
+
+ready(() => {
+  // Add the callback needed to update the text area character counters (e.g. for tweets)
+  document.querySelectorAll("textarea[data-max-length]").forEach(textArea => { textArea.addEventListener("keyup", updateCounter) });
+  polyfillDateAndTime();
 });
