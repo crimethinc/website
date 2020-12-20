bb9742e60c7e65103abe7f256a0f8170d7a95f59
Shane Becker <veganstraightedge@gmail.com>
2020-12-20 11:24:55 -0800

    Update IG link in footer

--- app/helpers/links_helper.rb
+++ app/helpers/links_helper.rb
@@ -1,14 +1,16 @@
 module LinksHelper
+  SOCIAL_LINKS = {
+    'CrimethInc. on Twitter'           => 'https://twitter.com/crimethinc',
+    'Crimethinc. on Facebook'          => 'https://www.facebook.com/To-Change-Everything-103734818122357',
+    'Crimethinc. on Instagram'         => 'https://instagram.com/CrimethincAgain',
+    'CrimethInc. on Github'            => 'https://github.com/crimethinc',
+    'CrimethInc. on Tumblr'            => 'https://crimethinc.tumblr.com',
+    'CrimethInc. on Mastodon'          => 'https://mastodon.online/@crimethinc',
+    'CrimethInc.com Articles RSS feed' => 'https://crimethinc.com/feed'
+  }.freeze
+
   def social_links
-    {
-      'CrimethInc. on Twitter'           => 'https://twitter.com/crimethinc',
-      'CrimethincDotCom on Facebook'     => 'https://www.facebook.com/To-Change-Everything-103734818122357',
-      'CrimethincDotCom on Instagram'    => 'https://instagram.com/CrimethincDotCom',
-      'CrimethInc on Github'             => 'https://github.com/crimethinc',
-      'CrimethInc on Tumblr'             => 'https://crimethinc.tumblr.com',
-      'CrimethInc on Mastodon'           => 'https://mastodon.online/@crimethinc',
-      'CrimethInc.com Articles RSS feed' => 'https://crimethinc.com/feed'
-    }
+    SOCIAL_LINKS
   end
 
   def social_link_classes url:, name:
