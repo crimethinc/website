e70d7bffdbe557113491b6dfb00e2cfd2dbd6489
astronaut-wannabe <contact@astronaut-wannabe.com>
2020-12-29 13:13:48 -0800

    [REFACTOR] DRY up the `for_index` scope

--- app/models/concerns/featureable.rb
+++ app/models/concerns/featureable.rb
@@ -6,6 +6,12 @@ module Featureable
     before_save :update_featured_at
   end
 
+  module ClassMethods
+    def for_index fallback_sort = { title: :asc }
+      where.not(hide_from_index: true).reorder(position: :asc).order(fallback_sort).english.published.live
+    end
+  end
+
   def update_featured_at
     self.featured_at = featured_status? ? Time.current : nil
   end
