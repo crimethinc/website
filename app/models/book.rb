7bbd7774ac67ebc361edec12c54bde50644cdf76
astronaut-wannabe <astronaut-wannabe@users.noreply.github.com>
2020-12-29 16:05:30 -0800

    [REFACTOR] DRY up the `for_index` scope (#1927)

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,8 +2,6 @@ class Book < ApplicationRecord
   include MultiPageTool
   include Featureable
 
-  scope :for_index, -> { where.not(hide_from_index: true).reorder(position: :asc).order(title: :asc) }
-
   def ask_for_donation?
     downloads_available?
   end
