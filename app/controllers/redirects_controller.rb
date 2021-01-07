c8cd711efec57393373bbd160151414f5bd5221c
Shane Becker <veganstraightedge@gmail.com>
2021-01-07 00:46:04 -0800

    Extract temp to private method

--- app/controllers/redirects_controller.rb
+++ app/controllers/redirects_controller.rb
@@ -1,12 +1,17 @@
 class RedirectsController < ApplicationController
   def show
-    redirect = Redirect.where(source_path: request.path.downcase).last
-    redirect = Redirect.where(source_path: "#{request.path.downcase}/").last if redirect.blank?
-
     if redirect.present?
-      redirect_to(redirect.target_path.downcase, status: redirect.temporary? ? :found : :moved_permanently)
+      redirect_to redirect.target_path.downcase, status: redirect.temporary? ? :found : :moved_permanently
     else
       render file: Rails.root.join('public/404.html'), status: :not_found
     end
+  end
+
+  private
+
+  def redirect
+    return @redirect if @redirect.present?
+
+    @redirect = Redirect.where(source_path: request.path.downcase).or(Redirect.where(source_path: "#{request.path.downcase}/")).last
   end
 end
