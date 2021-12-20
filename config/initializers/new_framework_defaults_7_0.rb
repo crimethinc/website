38979e3ceff69c404322cfaabb1b8a2b710dafd3
Shane Becker <veganstraightedge@gmail.com>
2021-12-19 19:05:13 -0800

    Enable remove_deprecated_time_with_zone_name default

--- config/initializers/new_framework_defaults_7_0.rb
+++ config/initializers/new_framework_defaults_7_0.rb
@@ -32,7 +32,7 @@ Rails.application.config.active_support.hash_digest_class = OpenSSL::Digest::SHA
 
 # Don't override ActiveSupport::TimeWithZone.name and use the default Ruby
 # implementation.
-# Rails.application.config.active_support.remove_deprecated_time_with_zone_name = true
+Rails.application.config.active_support.remove_deprecated_time_with_zone_name = true
 
 # Change the format of the cache entry.
 # Changing this default means that all new cache entries added to the cache
