2cee6151bfe3b1ce9503ca530374ce7da456b771
astronaut-wannabe <astronaut-wannabe@users.noreply.github.com>
2022-01-27 21:09:00 -0800

    Fix `issues#show` (#2383)

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -1,12 +1,29 @@
-class IssuesController < ToolsController
-  # See tools_controller.rb for inherited behavior
+class IssuesController < ApplicationController
+  before_action :set_html_id
+  before_action :set_body_id
+
   def show
-    journal   = Journal.find_by(slug: params[:slug])
-    @issue    = Issue.find_by(issue: params[:issue_number], journal_id: journal.id)
+    @type = 'issues'
+    journal   = Journal.find_by(slug: show_params[:slug])
+    @issue    = Issue.find_by(issue: show_params[:issue_number], journal_id: journal.id)
     @tool     = @issue
     @editable = @issue
 
     @title = PageTitle.new title_for :journals, journal.name, @issue.issue
     render "#{Current.theme}/books/show"
+  end
+
+  private
+
+  def set_html_id
+    @html_id = 'page'
+  end
+
+  def set_body_id
+    @body_id = 'tools'
+  end
+
+  def show_params
+    params.permit(:slug, :issue_number)
   end
 end
