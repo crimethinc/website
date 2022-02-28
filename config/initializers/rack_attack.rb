42891a40d3a18edd03615e9b0a1fa259ebc29252
Shane Becker <veganstraightedge@gmail.com>
2022-02-27 20:03:51 -0800

    Check env var presence

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -8,7 +8,11 @@ if Rails.env.production?
   module Rack
     class Attack
       throttle('limit requests per IP', limit: 60, period: 1.minute) do |req|
-        return if req.user_agent.starts_with? ENV.fetch('RACK_ATTACK_ALLOWED_USER_AGENT') { 'TODO' }
+        # Allow trusted requests through unthrottled
+        return if ENV['RACK_ATTACK_ALLOWED_USER_AGENT'].present? &&
+                  req.user_agent.starts_with?(ENV['RACK_ATTACK_ALLOWED_USER_AGENT'])
+
+        # Allow Asset Pipeline and ActiveStorage requests through unthrottled
         return if req.path.start_with? '/assets', '/rails/active_storage'
 
         req.ip
