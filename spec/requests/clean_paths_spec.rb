294fe01571d6b910a2e4b3d6af444f6570ca7f19
Justin ! <just1602@riseup.net>
2022-02-27 23:54:23 -0500

    Validate that sitemap.xml is not redirected
    I don't know why, but sitemap.xml is not redirected by our middleware. I
    thought I'd ahve to add `|| @req.path == '/sitemap.xml'` to the
    `exit_early?` method, but it's not the case.
    
    To validate I add the `public/sitemap.xml` file and I validate the I got
    the right content when I query `/sitemap.xml` and it works. The only
    things to keep in mind is since I create a dummy `public/sitemap.xml`
    file, we need to regenerate one every time we deploy the application.

--- spec/requests/clean_paths_spec.rb
+++ spec/requests/clean_paths_spec.rb
@@ -12,4 +12,12 @@ RSpec.describe 'Rack::CleanPath', type: :request do
 
     expect(response.header['Location']).to eq('/about/faq?test=true')
   end
+
+  it 'do not strip .xml extension for sitemap.xml' do
+    get 'http://example.com/sitemap.xml'
+
+    expect(response.status).to eq(200)
+    expect(response.header['Location']).to eq(nil)
+    expect(response.body).to eq('<sitemap>test</sitemap>')
+  end
 end
