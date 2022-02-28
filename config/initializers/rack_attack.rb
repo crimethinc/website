c6c56bae41f9b891548a96e8f6e28dd4f1da1785
Shane Becker <veganstraightedge@gmail.com>
2022-02-27 20:29:03 -0800

    Allow unmetered access sometimes (#2443)
    * Allow unmetered access sometimes
    
    * Check env var presence

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -8,7 +8,14 @@ if Rails.env.production?
   module Rack
     class Attack
       throttle('limit requests per IP', limit: 60, period: 1.minute) do |req|
-        req.ip unless req.path.start_with?('/assets', '/rails/active_storage')
+        # Allow trusted requests through unthrottled
+        return if ENV['RACK_ATTACK_ALLOWED_USER_AGENT'].present? &&
+                  req.user_agent.starts_with?(ENV['RACK_ATTACK_ALLOWED_USER_AGENT'])
+
+        # Allow Asset Pipeline and ActiveStorage requests through unthrottled
+        return if req.path.start_with? '/assets', '/rails/active_storage'
+
+        req.ip
       end
     end
   end
