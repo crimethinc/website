21796b31d465b4efef7d3fc15022a32ed2133eb5
Shane Becker <veganstraightedge@gmail.com>
2022-02-27 19:53:47 -0800

    Allow unmetered access sometimes

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -8,7 +8,10 @@ if Rails.env.production?
   module Rack
     class Attack
       throttle('limit requests per IP', limit: 60, period: 1.minute) do |req|
-        req.ip unless req.path.start_with?('/assets', '/rails/active_storage')
+        return if req.user_agent.starts_with? ENV.fetch('RACK_ATTACK_ALLOWED_USER_AGENT') { 'TODO' }
+        return if req.path.start_with? '/assets', '/rails/active_storage'
+
+        req.ip
       end
     end
   end
