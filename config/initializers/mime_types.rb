55b164ad5785b486774a3296ea8c49fe2595872e
Shane Becker <veganstraightedge@gmail.com>
2022-03-14 00:28:36 -0700

    Add route and admin button to download .docx of article (#2480)

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -4,3 +4,4 @@
 # Mime::Type.register "text/richtext", :rtf
 
 Mime::Type.register 'text/markdown', :markdown
+Mime::Type.register 'application/vnd.openxmlformats-officedocument.wordprocessingml.document', :docx
