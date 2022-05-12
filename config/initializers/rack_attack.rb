6ca8e18a2125e0d3c7238eb2349812fa1bff2f20
astronaut-wannabe <contact@astronaut-wannabe.com>
2022-05-12 14:12:31 -0400

    Update rack-attack config for login page

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -13,6 +13,18 @@ if Rails.env.production?
 
         req.ip unless trusted_request || asset_pipeline || active_storage
       end
+
+      # Lockout IP addresses that are hammering our login page.
+      # After 10 requests in 1 minute, block all requests from that IP for 1 hour.
+      Rack::Attack.blocklist('allow2ban login scrapers') do |req|
+        # `filter` returns false value if request is to your login page (but still
+        # increments the count) so request below the limit are not blocked until
+        # they hit the limit.  At that point, filter will return true and block.
+        Rack::Attack::Allow2Ban.filter(req.ip, maxretry: 10, findtime: 1.minute, bantime: 1.hour) do
+          # The count for the IP is incremented if the return value is truthy.
+          req.path == '/sessions' and req.post?
+        end
+      end
     end
   end
 end
