64b498e1d45bb4a809ac9fd6c4cf1f21916b936c
Justin ! <just1602@riseup.net>
2022-10-24 09:51:39 -0400

    Fix rubocop errors

--- spec/models/locale_spec.rb
+++ spec/models/locale_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Locale, type: :model do
   describe '#english?' do
-    let(:english_locale) { build :locale, :en }
-    let(:spanish_locale) { build :locale, :es }
+    let(:english_locale) { build(:locale, :en) }
+    let(:spanish_locale) { build(:locale, :es) }
 
     it 'checks for englishness' do
       expect(english_locale).to be_english
@@ -12,8 +12,8 @@ RSpec.describe Locale, type: :model do
   end
 
   describe '#display_name' do
-    let(:english_locale) { build :locale, :en }
-    let(:spanish_locale) { build :locale, :es }
+    let(:english_locale) { build(:locale, :en) }
+    let(:spanish_locale) { build(:locale, :es) }
 
     it 'formats for display' do
       expect(english_locale.display_name).to eq 'EN : English / English'
