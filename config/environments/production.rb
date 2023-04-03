e8b67fae7baffbea8ea0bf9a7430c0e458d6f908
Shane Becker <veganstraightedge@gmail.com>
2023-04-02 21:09:03 -0700

    Revert "Remove HDrive from ActiveStorage services (#3177)"
    This reverts commit 88cba9cc26627615abd59b16ccc3eef105dbcde3.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,7 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Store uploaded files in AWS S3 (see config/storage.yml for options).
-  config.active_storage.service = :amazon
+  config.active_storage.service = :production
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
