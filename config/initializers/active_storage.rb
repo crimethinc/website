756d6cf9ca71549d6957b0c7b1056003ab5b26bf
Shane Becker <veganstraightedge@gmail.com>
2023-04-11 06:59:26 -0700

    Enable proxy mode for ActiveStorage attachments

--- config/initializers/active_storage.rb
+++ config/initializers/active_storage.rb
@@ -1,1 +1,7 @@
 ActiveStorage::Engine.config.active_storage.content_types_to_serve_as_binary.delete 'image/svg+xml'
+
+# TODO: replace above line with this line below
+#       ActiveStorage::Engine is a private API
+# Rails.application.config.active_storage.content_types_to_serve_as_binary -= ['image/svg+xml']
+
+Rails.application.config.active_storage.resolve_model_to_route = :rails_storage_proxy
