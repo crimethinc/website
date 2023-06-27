a746577033ae4e4f8490f443e52ef4ed47e29104
Shane Becker <veganstraightedge@gmail.com>
2023-06-27 16:23:34 -0700

    Delete unused Status model (#3334)

--- app/models/status.rb
+++
@@ -1,7 +0,0 @@
-class Status < ApplicationRecord
-  has_many :articles, dependent: :destroy
-  has_many :books, dependent: :destroy
-  has_many :logos, dependent: :destroy
-  has_many :posters, dependent: :destroy
-  has_many :videos, dependent: :destroy
-end
