2d4bcae2f4b7fc19cdd42b6f6dd3c268bea80a5f
Shane Becker <veganstraightedge@gmail.com>
2023-08-27 12:08:58 -0700

    Add link to locale subdomain from /admin/locales

--- app/helpers/locales_helper.rb
+++ app/helpers/locales_helper.rb
@@ -1,6 +1,16 @@
 module LocalesHelper
+  def locale_subdomain_url locale
+    abbreviation = if locale.is_a? Locale
+                     locale.abbreviation
+                   else
+                     locale
+                   end
+
+    "https://#{abbreviation}.crimethinc.com"
+  end
+
   def locale abbreviation
-    Locale.find_by(abbreviation: abbreviation.downcase)
+    Locale.find_by abbreviation: abbreviation.downcase
   end
 
   def locale_nav_item_classes_for locale
