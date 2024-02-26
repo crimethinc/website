2f11faf59045be546ef093a880b181ceef1868e2
Shane Becker <veganstraightedge@gmail.com>
2024-02-25 16:32:38 -0800

    Revert schema file changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_10_09_024930) do
-  create_schema "heroku_ext"
-
+ActiveRecord::Schema[7.0].define(version: 2023_06_28_052836) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "pg_stat_statements"
   enable_extension "plpgsql"
 
   create_table "active_storage_attachments", force: :cascade do |t|
@@ -161,8 +158,8 @@ ActiveRecord::Schema[7.1].define(version: 2023_10_09_024930) do
     t.string "draft_code"
     t.string "slug"
     t.integer "publication_status"
-    t.datetime "published_at", precision: nil
-    t.datetime "featured_at", precision: nil
+    t.datetime "published_at"
+    t.datetime "featured_at"
     t.boolean "featured_status", default: false
     t.index ["canonical_id"], name: "index_definitions_on_canonical_id"
   end
