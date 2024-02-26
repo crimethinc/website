888722aaae6811bb2b1896e24ca453d5490fec0f
Shane Becker <veganstraightedge@gmail.com>
2024-02-25 19:51:40 -0800

    Layout/LineLength

--- app/models/concerns/publishable.rb
+++ app/models/concerns/publishable.rb
@@ -11,9 +11,30 @@ module Publishable
     scope :root,          -> { where(collection_id: nil) }
     scope :live,          -> { where('published_at < ?', Time.now.utc) }
     scope :recent,        -> { where('published_at BETWEEN ? AND ?', Time.now.utc - 2.days, Time.now.utc) }
-    scope :on,            ->(date) { where('published_at BETWEEN ? AND ?', date.try(:beginning_of_day), date.try(:end_of_day)) }
-    scope :next,          ->(article) { unscoped.root.where('published_at > ?', article.published_at).live.published.order(published_at: :asc).limit(1) }
-    scope :previous,      ->(article) { root.where('published_at < ?', article.published_at).live.published.chronological.limit(1) }
+
+    scope :on,
+          lambda { |date|
+            where('published_at BETWEEN ? AND ?', date.try(:beginning_of_day), date.try(:end_of_day))
+          }
+
+    scope :next,
+          lambda { |article|
+            unscoped.root
+                    .where('published_at > ?', article.published_at)
+                    .live
+                    .published
+                    .order(published_at: :asc)
+                    .limit(1)
+          }
+
+    scope :previous,
+          lambda { |article|
+            root.where('published_at < ?', article.published_at)
+                .live
+                .published
+                .chronological
+                .limit(1)
+          }
   end
 
   def dated?
