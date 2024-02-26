32d0a394114cda0d85ef200c889781320f69f761
Shane Becker <veganstraightedge@gmail.com>
2024-02-25 20:34:40 -0800

    Rails/RedundantActiveRecordAllMethod

--- app/models/support_session.rb
+++ app/models/support_session.rb
@@ -9,7 +9,7 @@ class SupportSession < ApplicationRecord
     def generate_token
       loop do
         new_token = SecureRandom.urlsafe_base64(nil, false)
-        break new_token unless all.exists?(token: new_token)
+        break new_token unless exists?(token: new_token)
       end
     end
   end
