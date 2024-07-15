2184f49fc5c33bcc6be5e432600918dbd4f5a736
Justin ! <just1602@riseup.net>
2024-07-15 13:23:23 -0400

    fix: rubocop errors

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -10,7 +10,7 @@ module ArticlesHelper
   def displayable_localizations article
     return article.localizations if signed_in?
 
-    article.localizations.filter_map { |localization| localization if localization.published? }
+    article.localizations.select(&:published?)
   end
 
   def story_card_background_image article:
