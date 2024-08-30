677cf2b3542bb868c16a74b18e8c817bf036ab30
Shane Becker <veganstraightedge@gmail.com>
2024-08-29 23:54:21 -0700

    Load defaults 7.2

--- config/initializers/new_framework_defaults_7_2.rb
+++
@@ -1,5 +0,0 @@
-Rails.application.config.active_job.enqueue_after_transaction_commit = :default
-Rails.application.config.active_record.validate_migration_timestamps = true
-Rails.application.config.yjit = true
-Rails.application.config.active_storage.web_image_content_types = %w[image/png image/jpeg image/gif image/webp]
-Rails.application.config.active_record.postgresql_adapter_decode_dates = true
