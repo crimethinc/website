6e6a9df6afe6c183ea48ca88a13f939dbb60187d
Shane Becker <veganstraightedge@gmail.com>
2024-08-29 23:12:28 -0700

    Enable active_storage.web_image_content_types webp framework default

--- config/initializers/new_framework_defaults_7_2.rb
+++ config/initializers/new_framework_defaults_7_2.rb
@@ -1,16 +1,7 @@
 Rails.application.config.active_job.enqueue_after_transaction_commit = :default
-
-###
-# TODO: investigate Heroku support for libvips + WebP
-# TODO: investigate which browsers can/not use WebP images
-# Adds image/webp to the list of content types Active Storage considers as an image
-# Prevents automatic conversion to a fallback PNG, and assumes clients support WebP, as they support gif, jpeg, and png.
-# This is possible due to broad browser support for WebP, but older browsers and email clients may still not support
-# WebP. Requires imagemagick/libvips built with WebP support.
-#++
-# Rails.application.config.active_storage.web_image_content_types = %w[image/png image/jpeg image/gif image/webp]
-
 Rails.application.config.active_record.validate_migration_timestamps = true
+Rails.application.config.yjit = true
+Rails.application.config.active_storage.web_image_content_types = %w[image/png image/jpeg image/gif image/webp]
 
 ###
 # Controls whether the PostgresqlAdapter should decode dates automatically with manual queries.
@@ -21,5 +12,3 @@ Rails.application.config.active_record.validate_migration_timestamps = true
 # This query used to return a `String`.
 #++
 # Rails.application.config.active_record.postgresql_adapter_decode_dates = true
-
-Rails.application.config.yjit = true
