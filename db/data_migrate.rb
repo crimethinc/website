c34de879292ac04a912b3407456cddebdee7777c
Shane Becker <veganstraightedge@gmail.com>
2024-09-09 19:48:40 -0700

    Rename locals in DataMigrate.run

--- db/data_migrate.rb
+++ db/data_migrate.rb
@@ -2,61 +2,63 @@
 class DataMigrate
   class << self
     def run
+      # locales
       Locale.find_each do |locale|
         locale.update! temp_language_direction: locale.language_direction
       end
 
-      User.find_each do |locale|
-        locale.update! temp_role: locale.role
+      # users
+      User.find_each do |user|
+        user.update! temp_role: user.role
       end
 
       # "publishable" tables
-      Article.find_each do |publishable|
-        publishable.update! temp_publication_status: publishable.publication_status
+      Article.find_each do |article|
+        article.update! temp_publication_status: article.publication_status
       end
 
-      Book.find_each do |publishable|
-        publishable.update! temp_publication_status: publishable.publication_status
+      Book.find_each do |book|
+        book.update! temp_publication_status: book.publication_status
       end
 
-      Definition.find_each do |publishable|
-        publishable.update! temp_publication_status: publishable.publication_status
+      Definition.find_each do |definition|
+        definition.update! temp_publication_status: definition.publication_status
       end
 
-      Episode.find_each do |publishable|
-        publishable.update! temp_publication_status: publishable.publication_status
+      Episode.find_each do |episode|
+        episode.update! temp_publication_status: episode.publication_status
       end
 
-      Issue.find_each do |publishable|
-        publishable.update! temp_publication_status: publishable.publication_status
+      Issue.find_each do |issue|
+        issue.update! temp_publication_status: issue.publication_status
       end
 
-      Journal.find_each do |publishable|
-        publishable.update! temp_publication_status: publishable.publication_status
+      Journal.find_each do |journal|
+        journal.update! temp_publication_status: journal.publication_status
       end
 
-      Logo.find_each do |publishable|
-        publishable.update! temp_publication_status: publishable.publication_status
+      Logo.find_each do |logo|
+        logo.update! temp_publication_status: logo.publication_status
       end
 
-      Page.find_each do |publishable|
-        publishable.update! temp_publication_status: publishable.publication_status
+      Page.find_each do |page|
+        page.update! temp_publication_status: page.publication_status
       end
 
-      Poster.find_each do |publishable|
-        publishable.update! temp_publication_status: publishable.publication_status
+      Poster.find_each do |poster|
+        poster.update! temp_publication_status: poster.publication_status
       end
 
-      Sticker.find_each do |publishable|
-        publishable.update! temp_publication_status: publishable.publication_status
+      Sticker.find_each do |sticker|
+        sticker.update! temp_publication_status: sticker.publication_status
       end
 
-      Video.find_each do |publishable|
-        publishable.update! temp_publication_status: publishable.publication_status
+      Video.find_each do |video|
+        video.update! temp_publication_status: video.publication_status
       end
 
-      Zine.find_each do |publishable|
-        publishable.update! temp_publication_status: publishable.publication_status
+      Zine.find_each do |zine|
+        zine.update! temp_publication_status: zine.publication_status
       end
     end
   end
