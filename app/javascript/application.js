92cf35227964ba82498f1f113b562d638e1ae19f
Shane Becker <veganstraightedge@gmail.com>
2024-11-04 13:09:29 -0800

    Import controllers *after* turbo

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,4 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "controllers"
-import "@hotwired/turbo-rails"
+// Configure your import map in config/importmap.rb.
+// Read more: https://github.com/rails/importmap-rails
+import "@hotwired/turbo-rails";
+import "controllers";
