b47dd641fca3e79db2130a59306d44c4e263c045
Shane Becker <veganstraightedge@gmail.com>
2024-11-04 01:09:04 -0800

    Move Danger Zone to turbo for confirm

--- config/importmap.rb
+++ config/importmap.rb
@@ -4,9 +4,12 @@ pin 'application', preload: true
 
 # Our JS files
 pin 'support'
-pin 'popper', to: 'popper.js'
+pin 'popper',    to: 'popper.js'
 pin 'bootstrap', to: 'bootstrap.min.js'
 
-pin '@hotwired/stimulus', to: 'stimulus.min.js', preload: true
+# Hotwired JS https://hotwired.dev
+pin '@hotwired/stimulus',         to: 'stimulus.min.js',     preload: true
 pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js', preload: true
+pin '@hotwired/turbo-rails',      to: 'turbo.min.js'
+
 pin_all_from 'app/javascript/controllers', under: 'controllers'
