43218199ae7484a02b527f74386aa95ea8a81b02
Shane Becker <veganstraightedge@gmail.com>
2024-11-05 19:39:36 -0800

    TEMP: remove turbo

--- config/importmap.rb
+++ config/importmap.rb
@@ -10,6 +10,5 @@ pin 'bootstrap', to: 'bootstrap.min.js'
 # Hotwired JS https://hotwired.dev
 pin '@hotwired/stimulus',         to: 'stimulus.min.js',     preload: true
 pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js', preload: true
-pin '@hotwired/turbo-rails',      to: 'turbo.min.js'
 
 pin_all_from 'app/javascript/controllers', under: 'controllers'
