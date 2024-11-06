14c70a428cfb48d8021eae56c7de38ad57523eb3
Shane Becker <veganstraightedge@gmail.com>
2024-11-05 19:41:54 -0800

    Preload turbo in importmap.rb (to fix CSS?)

--- config/importmap.rb
+++ config/importmap.rb
@@ -10,6 +10,6 @@ pin 'bootstrap', to: 'bootstrap.min.js'
 # Hotwired JS https://hotwired.dev
 pin '@hotwired/stimulus',         to: 'stimulus.min.js',     preload: true
 pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js', preload: true
-pin '@hotwired/turbo-rails',      to: 'turbo.min.js'
+pin '@hotwired/turbo-rails',      to: 'turbo.min.js',        preload: true
 
 pin_all_from 'app/javascript/controllers', under: 'controllers'
