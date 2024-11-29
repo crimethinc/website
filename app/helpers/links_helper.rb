d4ac86c765c88cfbcfabb9ed0ebbcfe66d5bec9a
Shane Becker <veganstraightedge@gmail.com>
2024-11-28 16:51:14 -0800

    Layout footer

--- app/helpers/links_helper.rb
+++ app/helpers/links_helper.rb
@@ -1,7 +1,7 @@
 module LinksHelper
   SOCIAL_LINKS = {
     'CrimethInc. on Mastodon'          => 'https://todon.eu/@CrimethInc',
-    'Crimethinc. on Facebook'          => 'https://www.facebook.com/To-Change-Everything-103734818122357',
+    'Crimethinc. on Facebook'          => 'https://facebook.com/To-Change-Everything-103734818122357',
     'Crimethinc. on Instagram'         => 'https://instagram.com/crimethincredux',
     'Crimethinc. on Bluesky'           => 'https://bsky.app/profile/crimethinc.com',
     'CrimethInc. on Github'            => 'https://github.com/crimethinc',
@@ -11,33 +11,31 @@ module LinksHelper
     'Crimethinc. on TikTok'            => 'https://tiktok.com/@crimethinc',
     'CrimethInc. on Peertube'          => 'https://kolektiva.media/a/crimethinc',
     'CrimethInc. on YouTube'           => 'https://youtube.com/@crimethincexworkerscollective',
-    'CrimethInc. on Reddit'            => 'https://www.reddit.com/r/CrimethInc/',
+    'CrimethInc. on Reddit'            => 'https://reddit.com/r/CrimethInc',
     'CrimethInc.com Articles RSS feed' => 'https://crimethinc.com/feed'
   }.freeze
 
   # For 2020 theme
   def social_links_by_domain
-    {
-      'Mastodon'  => 'https://todon.eu/@crimethinc',
-      'Facebook'  => 'https://www.facebook.com/To-Change-Everything-103734818122357',
-      'Instagram' => 'https://instagram.com/crimethincredux',
-      'Bluesky'   => 'https://bsky.app/profile/crimethinc.com',
-      'Github'    => 'https://github.com/crimethinc',
-      'Telegram'  => 'https://t.me/ExWorkers',
-      'Tumblr'    => 'https://crimethinc.tumblr.com',
-      'Bandcamp'  => 'https://crimethinc.bandcamp.com',
-      'TikTok'    => 'https://tiktok.com/@crimethinc',
-      'Peertube'  => 'https://kolektiva.media/a/crimethinc',
-      'YouTube'   => 'https://youtube.com/@crimethincexworkerscollective',
-      'Reddit'    => 'https://www.reddit.com/r/CrimethInc/',
-      'RSS feed'  => 'https://crimethinc.com/feed'
-    }
+    [
+      { Mastodon:   'https://todon.eu/@crimethinc' },
+      { Bluesky:    'https://bsky.app/profile/crimethinc.com' },
+      { Instagram:  'https://instagram.com/crimethincredux' },
+      { 'RSS feed': 'https://crimethinc.com/feed' },
+      { Facebook:   'https://facebook.com/To-Change-Everything-103734818122357' },
+      { Github:     'https://github.com/crimethinc' },
+      { Telegram:   'https://t.me/ExWorkers' },
+      { Tumblr:     'https://crimethinc.tumblr.com' },
+      { Bandcamp:   'https://crimethinc.bandcamp.com' },
+      { TikTok:     'https://tiktok.com/@crimethinc' },
+      { Peertube:   'https://kolektiva.media/a/crimethinc' },
+      { YouTube:    'https://youtube.com/@crimethincexworkerscollective' },
+      { Reddit:     'https://reddit.com/r/CrimethInc' }
+    ]
   end
 
   # For 2017 theme
-  def social_links
-    SOCIAL_LINKS
-  end
+  def social_links = SOCIAL_LINKS
 
   # For 2017 theme
   def social_link_classes url:, name:
