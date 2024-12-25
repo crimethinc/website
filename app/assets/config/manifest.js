9dcc976bc5928abed073e33388d4098aea8c8af4
Shane Becker <veganstraightedge@gmail.com>
2024-12-25 00:12:19 -0800

    Extract Bootstrap + partials from app.scss

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,12 +1,20 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
 
-//= link to_change_everything.css
+// /admin site
 //= link admin.css
-//= link steal-something-from-work-day.css
-//= link 2017.css
-//= link 2025.css
 
+// Mini sites
+//= link to_change_everything.css
+//= link steal-something-from-work-day.css
+
+// Legacy theme (2017)
+//= link 2017.css
+
+// 2025 theme
+//= link_directory ../stylesheets/2025 .css
+
+// TODO: delete this?
 // ??? lite_mode.css
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
