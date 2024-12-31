baa0d3675306de56c55e796ceb1f744053e3c4ff
Shane Becker <veganstraightedge@gmail.com>
2024-12-30 23:13:52 -0800

    Correct Heroku subdomain in domain redirects middleware

--- app/middlewares/rack/domain_redirect.rb
+++ app/middlewares/rack/domain_redirect.rb
@@ -11,7 +11,7 @@ module Rack
       ['', '/tce', /tochangeeverything\.com/],
 
       # Heroku subdomain
-      ['', '', /crimethinc\.herokuapp\.com$/],
+      ['', '', /\.herokuapp\.com$/],
 
       # Other TLDs
       ['', '', /crimethinc\.gay$/],
