32954c15268b901c878e910d4ae31d69074e74a5
Shane Becker <veganstraightedge@gmail.com>
2025-01-13 02:02:42 -0800

    Replace mini_magick with vips, adjust Logo

--- app/helpers/active_storage_helper.rb
+++ app/helpers/active_storage_helper.rb
@@ -1,22 +1,10 @@
 module ActiveStorageHelper
   def image_variant_by_width image, width
-    define = <<~DEFINE.squish
-      filter:support=2
-      jpeg:fancy-upsampling=off
-      png:compression-filter=5
-      png:compression-level=9
-      png:compression-strategy=1
-      png:exclude-chunk=all
-    DEFINE
-
-    image.variant filter:     'Triangle',
-                  define:     define,
-                  thumbnail:  width,
-                  unsharp:    '0.25x0.08+8.3+0.045',
-                  dither:     'None',
-                  posterize:  '136',
-                  quality:    '82',
-                  interlace:  'none',
-                  colorspace: 'sRGB'
+    image.variant resize_to_limit: [width, width],
+                  saver:           {
+                    strip:   true,
+                    quality: 50,
+                    shrink:  8
+                  }
   end
 end
