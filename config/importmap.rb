a210b23012caa1f6a57a0d977c2a997c31006d1b
Shane Becker <veganstraightedge@gmail.com>
2025-01-16 23:15:21 -0800

    Don't preload any JS in importmap.rb

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,13 +1,13 @@
 # Pin npm packages by running ./bin/importmap
 
-pin 'application', preload: true
+pin 'application', preload: false
 
 # Our JS files
-pin 'support'
-pin 'bootstrap', to: 'bootstrap.min.js'
+pin 'support', preload: false
+pin 'bootstrap', to: 'bootstrap.min.js', preload: false
 
 # Hotwired JS https://hotwired.dev
-pin '@hotwired/stimulus',         to: 'stimulus.min.js',     preload: true
-pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js', preload: true
+pin '@hotwired/stimulus',         to: 'stimulus.min.js', preload: false
+pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js', preload: false
 
-pin_all_from 'app/javascript/controllers', under: 'controllers'
+pin_all_from 'app/javascript/controllers', under: 'controllers', preload: false
