7dd19a4262a47ed68372471a85deae253d06d7b6
Shane Becker <veganstraightedge@gmail.com>
2025-01-17 00:40:45 -0800

    Reduce browser console warnings of preloaded, but unused Javascript (#4252)
    - added `preload: false` to all pinned Javascripts in `importmap.rb`
    - added Javascript to only the page/s where its needed by using
    `content_for :head`
    - moved `importmap` and `javascript_import_module` tags into a `yield
    :javascript` which only gets render if `content_for? :javascript`
    - removed Bootstrap JS everywhere except `/admin` layout
    - with a plan for how to remove it there too, because it's only used to
    make the nav responsive hamburger icon

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,13 +1,12 @@
 # Pin npm packages by running ./bin/importmap
 
-pin 'application', preload: true
+pin :application
 
 # Our JS files
-pin 'support'
-pin 'bootstrap', to: 'bootstrap.min.js'
+pin :support
 
-# Hotwired JS https://hotwired.dev
-pin '@hotwired/stimulus',         to: 'stimulus.min.js',     preload: true
-pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js', preload: true
+# Stimulus JS (part of Hotwire JS) https://stimulus.hotwired.dev
+pin '@hotwired/stimulus',         to: 'stimulus.min.js',     preload: false
+pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js', preload: false
 
-pin_all_from 'app/javascript/controllers', under: 'controllers'
+pin_all_from 'app/javascript/controllers', under: 'controllers', preload: false
