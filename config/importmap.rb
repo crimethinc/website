8fbe5fe4cbe3bc2b5d342506476e48a774fea235
Shane Becker <veganstraightedge@gmail.com>
2025-01-16 23:18:08 -0800

    Lint

--- config/importmap.rb
+++ config/importmap.rb
@@ -3,11 +3,11 @@
 pin 'application', preload: false
 
 # Our JS files
-pin 'support', preload: false
-pin 'bootstrap', to: 'bootstrap.min.js', preload: false
+pin 'support',   preload: false
+pin 'bootstrap', preload: false, to: 'bootstrap.min.js'
 
 # Hotwired JS https://hotwired.dev
-pin '@hotwired/stimulus',         to: 'stimulus.min.js', preload: false
+pin '@hotwired/stimulus',         to: 'stimulus.min.js',     preload: false
 pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js', preload: false
 
 pin_all_from 'app/javascript/controllers', under: 'controllers', preload: false
