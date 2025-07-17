ed16899ac42959496a4d475b3ae04677748cbaf5
Shane Becker <veganstraightedge@gmail.com>
2025-07-17 00:21:46 -0700

    Expand Poster#image and #meta_image to check for back images too

--- app/models/poster.rb
+++ app/models/poster.rb
@@ -10,19 +10,27 @@ class Poster < ApplicationRecord
   has_one_attached :image_back_color_download
   has_one_attached :image_back_black_and_white_download
 
+  # TEMP: duplicate of #image, for now (2025-07-17)
   def meta_image
     return if preferred_front_image.blank?
 
     Rails.application.routes.url_helpers.rails_blob_url preferred_front_image
   end
 
-  alias front_image image
+  # TEMP: duplicate of #meta_image, for now (2025-07-17)
+  def image
+    return if preferred_front_image.blank?
+
+    Rails.application.routes.url_helpers.rails_blob_url preferred_front_image
+  end
 
   private
 
   def preferred_front_image
-    return image_front_color_image if image_front_color_image.attached?
+    return image_front_color_image           if image_front_color_image.attached?
     return image_front_black_and_white_image if image_front_black_and_white_image.attached?
+    return image_back_color_image            if image_back_color_image.attached?
+    return image_back_black_and_white_image  if image_back_black_and_white_image.attached?
 
     nil
   end
