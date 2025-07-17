7b16825ffa5ac0b0371c7357da625fd772498db3
Shane Becker <veganstraightedge@gmail.com>
2025-07-16 23:21:32 -0700

    Fix Poster#meta_image URL for social share preview

--- app/models/poster.rb
+++ app/models/poster.rb
@@ -9,4 +9,20 @@ class Poster < ApplicationRecord
   has_one_attached :image_front_black_and_white_download
   has_one_attached :image_back_color_download
   has_one_attached :image_back_black_and_white_download
+
+  def meta_image
+    return if preferred_front_image.blank?
+
+    Rails.application.routes.url_helpers.rails_blob_url preferred_front_image
+  end
+  alias image meta_image
+
+  private
+
+  def preferred_front_image
+    return image_front_color_image if image_front_color_image.attached?
+    return image_front_black_and_white_image if image_front_black_and_white_image.attached?
+
+    nil
+  end
 end
