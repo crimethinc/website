aa57859f944118e1dcabe24008e6785a81fe0a66
Shane Becker <veganstraightedge@gmail.com>
2025-10-24 00:25:22 -0700

    Add another skipped test

--- spec/models/random_tool_spec.rb
+++ spec/models/random_tool_spec.rb
@@ -2,11 +2,20 @@ require 'rails_helper'
 
 RSpec.describe RandomTool do
   describe '#sample' do
-    it 'gets a random tool from the database', skip: 'Fix this test' do
-      # tool_classes = %w[Book Logo Poster Sticker Video Zine]
+    it 'gets a random tool from the database' do
+      # Create test data for each tool type
+      create(:book, publication_status: 'published', published_at: 1.day.ago)
+      create(:logo, publication_status: 'published', published_at: 1.day.ago)
+      create(:poster, publication_status: 'published', published_at: 1.day.ago)
+      create(:sticker, publication_status: 'published', published_at: 1.day.ago)
+      create(:video, publication_status: 'published', published_at: 1.day.ago)
+      create(:zine, publication_status: 'published', published_at: 1.day.ago)
 
-      # tool = RandomTool.sample
-      # expect(tool_classes).to include(tool.class.name)
+      tool_classes = %w[Book Logo Poster Sticker Video Zine]
+
+      tool = described_class.sample
+      expect(tool_classes).to include(tool.class.name)
+      expect(tool).to be_persisted
     end
   end
 end
