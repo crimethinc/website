b308d7c6f3b40067e9ad97e984f9faa55acf6926
Shane Becker <veganstraightedge@gmail.com>
2026-03-13 12:48:46 -0700

    Rubocop

--- app/models/active_storage_mirror_syncer.rb
+++ app/models/active_storage_mirror_syncer.rb
@@ -17,24 +17,22 @@ class ActiveStorageMirrorSyncer
       # Select services where file doesn't exist
       mirrors = all_services - services
 
-      # Open the local file (if one exists)
+      # Open the local file (if one exists) and upload to mirrors
       if services.any?(ActiveStorage::Service::DiskService)
         local_file_path = services.find do |service|
           service.is_a? ActiveStorage::Service::DiskService
         end.path_for blob.key
 
-        local_file = File.open(local_file_path)
-      end
-
-      # Upload local file to mirrors (if one exists)
-      if local_file.present?
-        mirrors.each do |mirror|
-          mirror.upload blob.key, local_file, checksum: blob.checksum
+        File.open(local_file_path) do |local_file|
+          mirrors.each do |mirror|
+            mirror.upload blob.key, local_file, checksum: blob.checksum
+          end
         end
+
+        next
       end
 
       # If no local file exists then download a remote file and upload it to the mirrors
-      next if local_file.present?
 
       services.first.open blob.key, checksum: blob.checksum do |temp_file|
         mirrors.each do |mirror|
