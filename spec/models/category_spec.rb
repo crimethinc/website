86b1756259fb1ad6c8f06a0d665760d5a8398e10
Shane Becker <veganstraightedge@gmail.com>
2026-03-21 21:20:11 -0700

    Add Category specs for path and strip_whitespace

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,6 +1,22 @@
 require 'rails_helper'
 
 describe Category do
+  describe '#path' do
+    it 'returns the category path' do
+      category = described_class.new(name: 'Technology', slug: 'technology')
+
+      expect(category.path).to eq '/categories/technology'
+    end
+  end
+
+  describe '#strip_whitespace' do
+    it 'strips whitespace from name' do
+      category = described_class.create!(name: '  Padded  ')
+
+      expect(category.name).to eq 'Padded'
+    end
+  end
+
   describe 'description' do
     it 'is optional' do
       category = described_class.new(name: 'no-description')
