fbc2ff6403953b35606124b4dd34ecf9c621238e
Shane Becker <veganstraightedge@gmail.com>
2026-03-21 19:31:14 -0700

    Extract system test helpers into support files (#5188)
    Follow up to https://github.com/crimethinc/website/pull/5186 and
    https://github.com/crimethinc/website/pull/5187
    
    Move login helper and Capybara driver config out of `spec_helper.rb`
    into dedicated files under `spec/support/`. Simplify `login_user` by
    dropping the unused argument.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,10 +2,6 @@ require 'simplecov'
 SimpleCov.start 'rails'
 
 RSpec.configure do |config|
-  config.before(:each, type: :system) do
-    driven_by :rack_test
-  end
-
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
@@ -17,16 +13,4 @@ RSpec.configure do |config|
   config.shared_context_metadata_behavior = :apply_to_host_groups
 
   config.example_status_persistence_file_path = 'tmp/rspec-failures.txt'
-end
-
-def login_user _user
-  visit '/signin'
-
-  within('main') do
-    fill_in 'username', with: 'user1'
-    fill_in 'password', with: 'c' * 31
-  end
-
-  click_link_or_button 'Sign In'
-  expect(page).to have_content 'Logged in!'
 end
