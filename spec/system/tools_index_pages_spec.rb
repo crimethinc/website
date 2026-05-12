5adb925ac2ff9ebd0e279582cc79329f0f0d00b9
Bargraph6 <193206525+Bargraph6@users.noreply.github.com>
2026-05-13 02:07:30 +0700

    specs: (tools_index_pages_spec) wrap examples in describe blocks
    * spec/system/tools_index_pages_spec.rb: wrap examples in a `describe` block.
    
    Without a containing block, either `describe` or `context`, it will be
    difficult to add additonal test contexts (e.g. "when there are no
    uploads").
    
    ---
    
    related-to: https://github.com/crimethinc/website/pull/5370
    related-to: https://github.com/crimethinc/website/issues/5305
    related-to: https://github.com/crimethinc/website/pull/5324

--- spec/system/tools_index_pages_spec.rb
+++ spec/system/tools_index_pages_spec.rb
@@ -6,76 +6,86 @@ describe 'Tools pages' do
   def test_image = fixture_file_upload 'spec/fixtures/files/test_image.jpg', 'image/jpeg'
   def test_pdf   = fixture_file_upload('spec/fixtures/files/test_pdf.pdf',   'image/pdf')
 
-  it 'renders published logos calling /logos' do
-    logo = create(:logo, :live, title: 'published')
-    logo.image_jpg.attach test_image
+  describe '/logos' do
+    it 'renders published logos calling /logos' do
+      logo = create(:logo, :live, title: 'published')
+      logo.image_jpg.attach test_image
 
-    create(:logo, :published, :not_live, title: 'not live')
-    create(:logo, :draft, title: 'draft')
+      create(:logo, :published, :not_live, title: 'not live')
+      create(:logo, :draft, title: 'draft')
 
-    visit :logos
+      visit :logos
 
-    expect(page).to have_text 'published'
-    expect(page).to have_no_text 'draft'
-    expect(page).to have_no_text 'not live'
+      expect(page).to have_text 'published'
+      expect(page).to have_no_text 'draft'
+      expect(page).to have_no_text 'not live'
+    end
   end
 
-  it 'renders published stickers calling /stickers' do
-    first_sticker = create(:sticker, :live, title: 'published')
-    second_sticker = create(:sticker, :not_live, title: 'not live')
-    third_sticker = create(:sticker, :draft, title: 'draft')
+  describe '/stickers' do
+    it 'renders published stickers calling /stickers' do
+      first_sticker = create(:sticker, :live, title: 'published')
+      second_sticker = create(:sticker, :not_live, title: 'not live')
+      third_sticker = create(:sticker, :draft, title: 'draft')
 
-    first_sticker.image_front_color_image.attach test_image
-    second_sticker.image_front_color_image.attach test_image
-    third_sticker.image_front_color_image.attach test_image
+      first_sticker.image_front_color_image.attach test_image
+      second_sticker.image_front_color_image.attach test_image
+      third_sticker.image_front_color_image.attach test_image
 
-    visit :stickers
+      visit :stickers
 
-    expect(page).to have_text 'published'
-    expect(page).to have_no_text 'draft'
-    expect(page).to have_no_text 'not live'
+      expect(page).to have_text 'published'
+      expect(page).to have_no_text 'draft'
+      expect(page).to have_no_text 'not live'
+    end
   end
 
-  it 'renders published zines calling /zines' do
-    create(:zine, :live, title: 'published')
-    create(:zine, :not_live, title: 'not live')
-    create(:zine, :draft, title: 'draft')
+  describe '/zines' do
+    it 'renders published zines calling /zines' do
+      create(:zine, :live, title: 'published')
+      create(:zine, :not_live, title: 'not live')
+      create(:zine, :draft, title: 'draft')
 
-    visit :zines
+      visit :zines
 
-    expect(page).to have_text 'published'
-    expect(page).to have_no_text 'draft'
-    expect(page).to have_no_text 'not live'
+      expect(page).to have_text 'published'
+      expect(page).to have_no_text 'draft'
+      expect(page).to have_no_text 'not live'
+    end
   end
 
-  it 'renders published posters calling /posters' do
-    first_poster = create(:poster, :live, title: 'published')
-    second_poster = create(:poster, :not_live, title: 'not live')
-    third_poster = create(:poster, :draft, title: 'draft')
+  describe '/posters' do
+    it 'renders published posters calling /posters' do
+      first_poster = create(:poster, :live, title: 'published')
+      second_poster = create(:poster, :not_live, title: 'not live')
+      third_poster = create(:poster, :draft, title: 'draft')
 
-    first_poster.image_front_color_download.attach test_pdf
-    first_poster.image_front_color_image.attach test_image
-    second_poster.image_front_color_download.attach test_pdf
-    second_poster.image_front_color_image.attach test_image
-    third_poster.image_front_color_download.attach test_pdf
-    third_poster.image_front_color_image.attach test_image
+      first_poster.image_front_color_download.attach test_pdf
+      first_poster.image_front_color_image.attach test_image
+      second_poster.image_front_color_download.attach test_pdf
+      second_poster.image_front_color_image.attach test_image
+      third_poster.image_front_color_download.attach test_pdf
+      third_poster.image_front_color_image.attach test_image
 
-    visit :posters
+      visit :posters
 
-    expect(page).to have_text 'published'
-    expect(page).to have_no_text 'draft'
-    expect(page).to have_no_text 'not live'
+      expect(page).to have_text 'published'
+      expect(page).to have_no_text 'draft'
+      expect(page).to have_no_text 'not live'
+    end
   end
 
-  it 'renders published videos calling /videos' do
-    create(:video, :live, title: 'published')
-    create(:video, :not_live, title: 'not live')
-    create(:video, :draft, title: 'draft')
+  describe '/videos' do
+    it 'renders published videos calling /videos' do
+      create(:video, :live, title: 'published')
+      create(:video, :not_live, title: 'not live')
+      create(:video, :draft, title: 'draft')
     
-    visit :videos
+      visit :videos
 
-    expect(page).to have_text 'published'
-    expect(page).to have_no_text 'not live'
-    expect(page).to have_no_text 'draft'
+      expect(page).to have_text 'published'
+      expect(page).to have_no_text 'not live'
+      expect(page).to have_no_text 'draft'
+    end
   end
 end
