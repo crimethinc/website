bdbbea51e4279069d9105897cd664d746fe88ebf
Bargraph6 <193206525+Bargraph6@users.noreply.github.com>
2026-05-13 03:41:16 +0700

    specs: (tools_index_pages_spec) move before block to shared example
    * spec/system/tools_index_pages_spec.rb: move all the setup code to the share example.
    
    Now that we are using the uploads trait, we have the same exact setup
    code being used across every tool type.
    
    Moving it to the shared example gaurantees every tool type has the
    same setup, and if we want to add additional test `context` blocks we
    can easily vary the setup across tool types.
    
    ---
    
    related-to: https://github.com/crimethinc/website/pull/5370
    related-to: https://github.com/crimethinc/website/issues/5305
    related-to: https://github.com/crimethinc/website/pull/5324

--- spec/system/tools_index_pages_spec.rb
+++ spec/system/tools_index_pages_spec.rb
@@ -4,13 +4,19 @@ describe 'Tools pages' do
   before {  Current.theme = '2017' }
 
   def test_image = fixture_file_upload 'spec/fixtures/files/test_image.jpg', 'image/jpeg'
-  def test_pdf   = fixture_file_upload('spec/fixtures/files/test_pdf.pdf',   'image/pdf')
+  def test_pdf   = fixture_file_upload 'spec/fixtures/files/test_pdf.pdf',   'image/pdf'
 
   shared_examples "renders published tool type for the path" do |tool|
-    let(:tool) { tool.to_sym }
+    let(:uploads) { [] }
 
-    it "renders published logos calling /#{tool}" do
-      visit tool
+    before do
+      create(tool, :live, title: 'published', uploads: uploads)
+      create(tool, :published, :not_live, title: 'not live', uploads: uploads)
+      create(tool, :draft, title: 'draft', uploads: uploads)
+    end
+
+    it "renders published logos calling /#{tool.to_s.pluralize}" do
+      visit tool.to_s.pluralize.to_sym
 
       expect(page).to have_text 'published'
       expect(page).to have_no_text 'draft'
@@ -19,67 +25,37 @@ describe 'Tools pages' do
   end
 
   describe '/logos' do
-    let(:uploads) { [[:image_jpg, test_image]] }
-
-    before do
-      create(:logo, :live, title: 'published', uploads: uploads)
-      create(:logo, :published, :not_live, title: 'not live', uploads: uploads)
-      create(:logo, :draft, title: 'draft', uploads: uploads)
+    it_behaves_like "renders published tool type for the path", :logo do
+      let(:uploads) { [[:image_jpg, test_image]] }
     end
-
-    it_behaves_like "renders published tool type for the path", :logos
   end
 
   describe '/stickers' do
-    let(:uploads) { [[:image_front_color_image, test_image]] }
-
-    before do
-      create(:sticker, :live, title: 'published', uploads: uploads)
-      create(:sticker, :not_live, title: 'not live', uploads: uploads)
-      create(:sticker, :draft, title: 'draft', uploads: uploads)
+    it_behaves_like "renders published tool type for the path", :sticker do
+      let(:uploads) { [[:image_front_color_image, test_image]] }
     end
-
-    it_behaves_like "renders published tool type for the path", :stickers
   end
 
   describe '/zines' do
-    let(:uploads) { [] }
-
-    before do
-      create(:zine, :live, title: 'published', uploads: uploads)
-      create(:zine, :not_live, title: 'not live', uploads: uploads)
-      create(:zine, :draft, title: 'draft', uploads: uploads)
+    it_behaves_like "renders published tool type for the path", :zine do
+      let(:uploads) { [] }
     end
-
-    it_behaves_like "renders published tool type for the path", :zines
   end
 
   describe '/posters' do
-    let(:uploads) do
-      [
-        [:image_front_color_download, test_pdf],
-        [:image_front_color_image, test_image]
-      ]
+    it_behaves_like "renders published tool type for the path", :poster do
+      let(:uploads) do
+        [
+          [:image_front_color_download, test_pdf],
+          [:image_front_color_image, test_image]
+        ]
+      end
     end
-
-    before do
-      create(:poster, :live, title: 'published', uploads: uploads)
-      create(:poster, :not_live, title: 'not live', uploads: uploads)
-      create(:poster, :draft, title: 'draft', uploads: uploads)
-    end
-
-    it_behaves_like "renders published tool type for the path", :posters
   end
 
   describe '/videos' do
-    let(:uploads) { [[:image_poster_frame, test_image]] }
-
-    before do
-      create(:video, :live, title: 'published', uploads: uploads)
-      create(:video, :not_live, title: 'not live', uploads: uploads)
-      create(:video, :draft, title: 'draft', uploads: uploads)
+    it_behaves_like "renders published tool type for the path", :video do
+      let(:uploads) { [[:image_poster_frame, test_image]] }
     end
-
-    it_behaves_like "renders published tool type for the path", :videos
   end
 end
