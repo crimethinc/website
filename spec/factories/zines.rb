4f3930f4e87f7b80b6282a27c65ca7ed09530741
Bargraph6 <193206525+Bargraph6@users.noreply.github.com>
2026-05-17 18:27:55 +0700

    specs: move locale record creation to an optional trait
    * spec/factories/tools.rb: copied the locale creation logic from zine, but wrapped in a trait.
    * spec/factories/zines.rb: moved locale creation logic to parent tool factory.
    
    I was working on refactoring a database query, and to test if there
    was any performance improvement I modified a test to create 10,000
    records of each tool type.
    
    This made the spec take about 280 seconds to run. I discovered that
    most of this is spent in the `Zine` factory's `Locale` creation logic,
    which I didn't need for this particular spec.
    
    Performance
    ==================
    
    There isn't a difference with the current specs, as we don't often
    create a huge number of records for test data. However I didn't see
    any specs fail by making this change and it will unblock more
    aggressive testing on larger data sets if we have the need in the
    future.[^1]
    
    Before
    ------------------
    
    ```sh
    Finished in 17.21 seconds (files took 2.35 seconds to load)
    329 examples, 0 failures
    
    Randomized with seed 44528
    
    real	0m20.593s
    user	0m19.954s
    sys	0m2.416s
    ```
    
    After
    ------------------
    
    ```sh
    Finished in 17.02 seconds (files took 2.35 seconds to load)
    329 examples, 0 failures
    
    Randomized with seed 44528
    
    real	0m20.410s
    user	0m19.956s
    sys	0m2.429s
    ```
    
    [^1]: It is useful for local development. For example, being able to
          easily create a more realistic sized database can help for local
          testing of database query changes, even if there is no intention to
          check in a spec that creates 10,000 records into `main`

--- spec/factories/zines.rb
+++ spec/factories/zines.rb
@@ -1,13 +1,5 @@
 FactoryBot.define do
   factory :zine, parent: :tool, class: 'Zine' do
     locale { 'en' }
-
-    after(:build) do |tool|
-      locale = Locale.find_by(abbreviation: tool.locale)
-      if locale.blank?
-        trait_sym = tool.locale.underscore.to_sym
-        create(:locale, trait_sym)
-      end
-    end
   end
 end
