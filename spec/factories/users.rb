3ca9bf8ff6b33ac4b835d2443bd51c1c30d9f196
Justin ! <just1602@riseup.net>
2026-05-23 01:20:48 -0400

    spec: refactor user_spec

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,5 +2,13 @@ FactoryBot.define do
   factory :user do
     sequence(:username) { |n| "user#{n}" }
     password { 'x' * 30 }
+
+    trait :publisher do
+      role { 'publisher' }
+    end
+
+    trait :author do
+      role { 'author' }
+    end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,52 +1,60 @@
 require 'rails_helper'
 
 RSpec.describe User do
-  describe '#name' do
-    it 'returns the username' do
-      user = described_class.new(username: 'example')
+  subject(:user) { user }
 
-      expect(user.name).to eq 'example'
+  describe '#name' do
+    describe 'returns the username' do
+      subject { user.name }
+
+      let(:user) { build(:user, username: 'example') }
+
+      it { is_expected.to eq 'example' }
     end
   end
 
   describe '#strip_whitespace' do
-    it 'strips leading and trailing whitespace from username' do
-      user = create(:user, username: '  padded  ', password: 'x' * 30)
+    context 'when there is leading and trailing whitespace in a username' do
+      subject { user.username }
 
-      expect(user.username).to eq 'padded'
+      let(:user) { create(:user, username: '  padded  ') }
+
+      it { is_expected.to eq 'padded' }
     end
   end
 
   describe 'role permissions' do
-    it 'allows publishers to publish' do
-      user = build(:user, role: 'publisher')
+    context 'when the user is a publisher' do
+      let(:user) { build(:user, :publisher) }
 
-      expect(user.can_publish?).to be true
-      expect(user.cant_publish?).to be false
-      expect(user.can_delete?).to be true
-      expect(user.can_admin_users?).to be true
+      it { expect(user.can_publish?).to be true }
+      it { expect(user.cant_publish?).to be false }
+      it { expect(user.can_delete?).to be true }
+      it { expect(user.can_admin_users?).to be true }
     end
 
-    it 'does not allow authors to publish' do
-      user = build(:user, role: 'author')
+    context 'when the user is an author' do
+      let(:user) { build(:user, :author) }
 
-      expect(user.can_publish?).to be false
-      expect(user.cant_publish?).to be true
+      it { expect(user.can_publish?).to be false }
+      it { expect(user.cant_publish?).to be true }
     end
   end
 
   describe 'validations' do
-    it 'rejects passwords shorter than minimum length' do
-      user = build(:user, password: 'short')
+    context 'when passwords shorter than minimum length' do
+      let(:user) { build(:user, password: 'short') }
 
-      expect(user).not_to be_valid
-      expect(user.errors[:password]).to be_present
+      it 'is expected to be invalid and properly set the error message' do
+        expect(user).not_to be_valid
+        expect(user.errors[:password]).to be_present
+      end
     end
 
-    it 'rejects excluded passphrases' do
-      user = build(:user, password: 'mickey fickey fire cracker soap on a rope')
+    context 'when excluded passphrases' do
+      let(:user) { build(:user, password: 'mickey fickey fire cracker soap on a rope') }
 
-      expect(user).not_to be_valid
+      it { is_expected.not_to be_valid }
     end
   end
 end
